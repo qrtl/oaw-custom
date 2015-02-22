dbece968d12500c6280d56f6e5e7fddd4d342b5d
Yoshi Tashiro <tashiro@roomsfor.hk>
2015-02-22 23:54:51 +0800

    [ADD] product_listprice_list_view

---
+++ product_listprice_list_view/__openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) Rooms For (Hong Kong) Limited T/A OSCG (<http://www.openerp-asia.net>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Product List Price Update List View',
+    'version': '0.5',
+    'author': 'Rooms For (Hong Kong) Ltd T/A OSCG',
+    'website': 'http://www.openerp-asia.net',
+    'category': 'Product',
+    'depends': ["product", "sale", "stock"],
+    'description': """
+* Adds a menu item 'Product List Price' to facilitate list price update
+    """,
+    'data': [
+        'product_view.xml',
+    ],
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
