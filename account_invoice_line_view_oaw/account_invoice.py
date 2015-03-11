7c6de1950f06c172bb2b6f27e4e182655da7ed29
Yoshi Tashiro <tashiro@roomsfor.hk>
2015-03-11 20:48:32 +0800

    [IMP] new api

--- account_invoice_line_view_oaw/account_invoice.py
+++ account_invoice_line_view_oaw/account_invoice.py
@@ -19,152 +19,131 @@
 #
 ##############################################################################
 
-from openerp.osv import fields, osv
+from openerp import models, fields, api, _
 from datetime import datetime
 from openerp.tools.translate import _
 import openerp.addons.decimal_precision as dp
+ 
 
-
-class account_invoice_line(osv.osv):
+class InvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
 
-    def _get_ref_vals(self, cr, uid, ids, inv_ln, context=None):
-        so_id = 0
-        po_id = 0
-        SO = self.pool.get('sale.order')
-        PO = self.pool.get('purchase.order')
-        if inv_ln.invoice_id.reference:
-            if inv_ln.invoice_id.type == 'out_invoice':
-                so_id = SO.search(cr, uid, [('name','=',inv_ln.invoice_id.reference)])[0]
-            if inv_ln.invoice_id.type == 'in_invoice':
-                po_id = PO.search(cr, uid, [('name','=',inv_ln.invoice_id.reference)])[0]
-        res = {
-            'so_id': so_id,
-            'po_id': po_id,
-            }
-        return res
-        
-    def _get_vals(self, cr, uid, ids, field_names, args, context=None):
-        res = {}
-        for inv_ln in self.browse(cr, uid, ids, context=context):
-            ref_vals = self._get_ref_vals(cr, uid, ids, inv_ln, context=context)
-            res[inv_ln.id] = {
-                'partner_id': inv_ln.invoice_id.partner_id.id,
-                'state': inv_ln.invoice_id.state,
-                'date_invoice': inv_ln.invoice_id.date_invoice,
-                'ref': inv_ln.invoice_id.partner_id.ref,
-                'so_id': ref_vals['so_id'],
-                'po_id': ref_vals['po_id'],
-                }
-        return res
-    
-    def _get_base_amt(self, cr, uid, ids, field_names, args, context=None):
-        res = {}
-        Invoice = self.pool.get('account.invoice')
-        Rate = self.pool['res.currency.rate']
-        for inv_ln in self.browse(cr, uid, ids, context=context):
+    @api.model
+    def _get_org_vals(self, inv_ln):
+        so_id, po_id = 0, 0
+        SO = self.env['sale.order']
+        PO = self.env['purchase.order']
+        if inv_ln.invoice_id.origin:
+            if inv_ln.invoice_id.type == 'out_invoice' and \
+                SO.search([('name','=',inv_ln.invoice_id.origin)]):
+                so_id = \
+                    SO.search([('name','=',inv_ln.invoice_id.origin)])[0].id
+            if inv_ln.invoice_id.type == 'in_invoice' and \
+                PO.search([('name','=',inv_ln.invoice_id.origin)]):
+                po_id = \
+                    PO.search([('name','=',inv_ln.invoice_id.origin)])[0].id
+        return so_id, po_id
+         
+    @api.multi
+    @api.depends('invoice_id.state','invoice_id.origin')
+    def _get_vals(self):
+        for inv_ln in self:
+            inv_ln.so_id, inv_ln.po_id = self._get_org_vals(inv_ln)
+
+    @api.multi
+    def _get_base_amt(self):
+        Invoice = self.env['account.invoice']
+        Rate = self.env['res.currency.rate']
+        for inv_ln in self:
             curr_amt = inv_ln.price_subtotal
             # set rate 1.0 if the transaction currency is the same as the base currency
-            if inv_ln.company_id.currency_id == inv_ln.currency_id:
+            if inv_ln.currency_id == inv_ln.company_id.currency_id:
                 rate = 1.0
             else:
-                invoice_date = Invoice.browse(cr, uid, [inv_ln.invoice_id.id])[0].date_invoice
-                if not invoice_date:
-                    invoice_date = context.get('date', datetime.today().strftime('%Y-%m-%d'))
-                rate_id = Rate.search(cr, uid, [
+                invoice_date = Invoice.browse([inv_ln.invoice_id.id])[0].date_invoice \
+                    or inv_ln.env.context.get('date', datetime.today().strftime('%Y-%m-%d'))
+                rate = Rate.search([
                     ('currency_id', '=', inv_ln.currency_id.id),
                     ('name', '<=', invoice_date),
-                    ], order='name desc', limit=1, context=context)
-                if rate_id:
-                    rate = Rate.browse(cr, uid, rate_id, context=context)[0].rate
-                else:
-                    rate = 1.0
-            res[inv_ln.id] = {
-                'rate': rate,
-                'base_amt': curr_amt / rate,
-                }
-        return res
-
-    """ return all the invoice lines for the updated invoice """
-    def _get_invoice_lines(self, cr, uid, ids, context=None):
-        inv_ln_ids = []
-        for invoice in self.pool.get('account.invoice').browse(cr, uid, ids, context=context):
-            inv_ln_ids += \
-                self.pool.get('account.invoice.line').search(cr, uid,
-                [('invoice_id.id', 'in', ids)], context=context)
-        return inv_ln_ids
+                    ], order='name desc', limit=1).rate or 1.0
+            inv_ln.rate = rate
+            inv_ln.base_amt = curr_amt / rate
 
 
-    _order = 'id desc'
-    """ some fields are defined with 'store' for grouping purpose """
-    _columns ={
-        'user_id': fields.related('invoice_id', 'user_id',type='many2one',relation='res.users',string=u'Salesperson'),
-        'number': fields.related('invoice_id', 'number',type='char',relation='account.move',string=u'Number'),
-        'partner_id': fields.function(_get_vals, type='many2one', obj='res.partner', string=u'Partner',
-            store={
-                   'account.invoice.line': (lambda self, cr, uid, ids, c={}: ids, None, 10),
-                   'account.invoice': (_get_invoice_lines, ['partner_id'], 10),
-                   },
-            multi='invoice',
-            ),
-        'state': fields.function(_get_vals, type='char', string=u'Status',
-            store={
-                   'account.invoice.line': (lambda self, cr, uid, ids, c={}: ids, None, 10),
-                   'account.invoice': (_get_invoice_lines, ['state'], 10),
-                   },
-            multi='invoice',
-            ),
-        'date_invoice': fields.function(_get_vals, type='date', string=u'Invoice Date',
-            store={
-                   'account.invoice.line': (lambda self, cr, uid, ids, c={}: ids, None, 10),
-                   'account.invoice': (_get_invoice_lines, ['date_invoice'], 10), 
-                   },
-            multi='invoice',
-            ),
-        'ref': fields.function(_get_vals, type='char', string=u'Partner Ref',
-            store={
-                   'account.invoice.line': (lambda self, cr, uid, ids, c={}: ids, None, 10),
-                   'account.invoice': (_get_invoice_lines, ['partner_id'], 10),
-                   'res.partner': (_get_invoice_lines, ['ref'], 10),
-                   },
-            multi='invoice',
-            ),
-        'period_id': fields.related('invoice_id', 'period_id', type='many2one', relation='account.period', string=u'Period'),
-        'reference': fields.related('invoice_id','reference',type='char',string=u'Invoice Ref'),
-        'date_due': fields.related('invoice_id','date_due',type='date',string=u'Due Date'),
-        'currency_id': fields.related('invoice_id','currency_id',relation='res.currency', type='many2one',string=u'Currency'),
-        'rate': fields.function(_get_base_amt, type='float', string=u'Rate', multi='base_amt'),
-        'base_amt': fields.function(_get_base_amt, type='float', digits_compute=dp.get_precision('Account'), string=u'Base Amount', multi="base_amt"),
-        'so_id': fields.function(_get_vals, type='many2one',
-            obj='sale.order', string='SO',
-            store={
-                   'account.invoice.line': (lambda self, cr, uid, ids, c={}: ids, None, 10),
-                   'account.invoice': (_get_invoice_lines, ['state'], 10),
-                   },
-            multi="invoice"
-            ),
-        'po_id': fields.function(_get_vals, type='many2one',
-            obj='purchase.order', string='PO',
-            store={
-                   'account.invoice.line': (lambda self, cr, uid, ids, c={}: ids, None, 10),
-                   'account.invoice': (_get_invoice_lines, ['state'], 10),
-                   },
-            multi="invoice"
-            ),
-        }
+    user_id = fields.Many2one('res.users',
+                              related='invoice_id.user_id',
+                              store=True,
+                              readonly=True,
+                              string='Salesperson'
+                              )
+    number = fields.Char(related='invoice_id.number',
+                         store=True,
+                         readonly=True,
+                         string='Number'
+                         )
+    state = fields.Selection(related='invoice_id.state',
+                             store=True,
+                             readonly=True,
+                             string='Status'
+                             )
+    date_invoice = fields.Date(related='invoice_id.date_invoice',
+                               store=True,
+                               readonly=True,
+                               string='Invoice Date'
+                               )
+    ref = fields.Char(related='invoice_id.partner_id.ref',
+                      store=True,
+                      readonly=True,
+                      string='Partner Ref'
+                      )
+    period_id = fields.Many2one(related='invoice_id.period_id',
+                                readonly=True,
+                                string='Period'
+                                )
+    reference = fields.Char(related='invoice_id.reference',
+                            readonly=True,
+                            string='Invoice Ref'
+                            )
+    date_due = fields.Date(related='invoice_id.date_due',
+                           readonly=True,
+                           string='Due Date'
+                           )
+    currency_id = fields.Many2one(related='invoice_id.currency_id',
+                                  readonly=True,
+                                  string='Currency'
+                                  )
+    rate = fields.Float(compute='_get_base_amt',
+                        readonly=True,
+                        string='Rate'
+                        )
+    base_amt = fields.Float(compute='_get_base_amt',
+                            digits_compute=dp.get_precision('Account'),
+                            readonly=True,
+                            string='Base Amount'
+                            )
+    so_id = fields.Many2one('sale.order',
+                            compute='_get_vals',
+                            store=True,
+                            readonly=True,
+                            string='SO'
+                            )
+    po_id = fields.Many2one('purchase.order',
+                            compute='_get_vals',
+                            store=True,
+                            readonly=True,
+                            string='PO'
+                            )
 
+ 
     def init(self, cr):
         # to be executed only when installing the module.  update "stored" fields 
         sql = """
             update account_invoice_line l
             set state = inv.state,
                 date_invoice = inv.date_invoice,
-                partner_id = inv.partner_id,
                 ref = p.ref
             from account_invoice inv
             left join res_partner p on inv.partner_id = p.id
             where l.invoice_id = inv.id
             """
         cr.execute(sql)
-
-account_invoice_line()
