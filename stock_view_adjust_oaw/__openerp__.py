c6c41b386efcd312a5ceef7013ed3917825f6177
Yoshi Tashiro <tashiro@roomsfor.hk>
2016-05-09 11:41:11 -0400

    [IMP] rename sale_order_type to sale_line_quant_extended, add is_mto to picking

---
+++ stock_view_adjust_oaw/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2015-2016 Rooms For (Hong Kong) Limited T/A OSCG
+#    <https://www.odoo-asia.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+{
+    'name': 'Stock View Adjust OAW',
+    'summary': '',
+    'version': '8.0.0.7.0',
+    'category': 'Stock',
+    'author': 'Rooms For (Hong Kong) Limited T/A OSCG',
+    'website': 'https://www.odoo-asia.com',
+    'description': """
+* Adds menu item Stock Move (Ext.)
+    """,
+    "license": "AGPL-3",
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'images' : [],
+    'depends': ['sale_line_quant_extended',
+                ],
+    'data': [
+         'stock_view.xml',        
+    ],
+    'test': [],
+    'demo': [],
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
