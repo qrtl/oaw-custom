99d1d80d7b3676b18eb0185332ffdc759631c634
Yoshi Tashiro <tashiro@roomsfor.hk>
2016-05-17 23:55:38 -0400

    [ADD] cron_purchase_confirm

---
+++ cron_purchase_confirm/models/purchase.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2016 Rooms For (Hong Kong) Limited T/A OSCG
+#    <https://www.odoo-asia.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from openerp import models, api, _, fields
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    @api.model
+    def confirm_purchase_order(self):
+        orders = self.search([('is_vci','=',True),('state','=','draft')])
+        for order in orders:
+            order.signal_workflow('purchase_confirm')
+        return True
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
