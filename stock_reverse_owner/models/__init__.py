309cd6e1dc426643536003d916ac7298af307afc
Yoshi Tashiro <tashiro@roomsfor.hk>
2016-10-26 12:01:39 -0400

    [FIX] revive create invoice button, remove return vci option

--- stock_reverse_owner/models/__init__.py
+++ stock_reverse_owner/models/__init__.py
@@ -2,7 +2,7 @@
 # Copyright 2016 Rooms For (Hong Kong) Limited T/A OSCG
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import stock_return_picking_hook
+# import stock_return_picking_hook
 import stock_picking
 import stock_location
 import stock_quant_hook_move_quants_write
--- stock_reverse_owner/wizard/__init__.py
+++ stock_reverse_owner/wizard/__init__.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 
-import stock_return_picking
-import stock_transfer_details
+from . import stock_return_picking_hook
+from . import stock_return_picking
+from . import stock_transfer_details
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
