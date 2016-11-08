5083c49402e5fb173c6db7d2d2c343eb684ae57b
Yoshi Tashiro <tashiro@roomsfor.hk>
2016-11-08 04:29:03 -0500

    [FIX] module version

--- stock_consignment_report/__openerp__.py
+++ stock_consignment_report/__openerp__.py
@@ -4,7 +4,7 @@
 {
     "name": "Consignment Report",
     "summary": "",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "category": "Reporting",
     "website": "https://www.odoo-asia.com/",
     "author": "Rooms For (Hong Kong) Limited T/A OSCG",
