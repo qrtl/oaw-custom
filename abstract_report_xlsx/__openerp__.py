8df02ff0afaa9eac148c27d2e482d60f077fc5e7
Yoshi Tashiro <tashiro@roomsfor.hk>
2016-11-19 11:06:33 -0500

    [ADD] stock_offer_report, abstract_report_xlsx

---
+++ abstract_report_xlsx/__openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Author: Julien Coux
+# Copyright 2016 Camptocamp SA
+# Copyright 2016 Rooms For (Hong Kong) Limited T/A OSCG
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Abstract Report XLSX",
+    "summary": "",
+    "version": "8.0.1.0.0",
+    "category": "Reporting",
+    "website": "https://www.odoo-asia.com/",
+    "author": "Rooms For (Hong Kong) Limited T/A OSCG",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "report_xlsx",
+    ],
+    "data": [
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ]
+}
