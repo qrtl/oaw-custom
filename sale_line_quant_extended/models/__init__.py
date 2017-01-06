33e7fc23f0331ded3726704dc62c821ed303252b
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-01-06 13:40:45 +0800

    [IMP] add supplier to search definition

--- sale_line_quant_extended/models/__init__.py
+++ sale_line_quant_extended/models/__init__.py
@@ -16,7 +16,8 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from . import sale
+from . import sale_order
+from . import sale_order_line
 from . import procurement
 from . import stock
 from . import purchase
