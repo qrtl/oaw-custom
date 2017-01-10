4bb596b8f046aa96236fb48de4f904b41aeca0cf
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-01-10 23:08:07 +0800

    [ADD] supplier_stock

---
+++ supplier_stock/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Rooms For (Hong Kong) Limted T/A OSCG
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Supplier Stock',
+    'category': 'Stock',
+    'version': '8.0.1.0.0',
+    'author': 'Rooms For (Hong Kong) Limited T/A OSCG',
+    'website': 'https://www.odoo-asia.com',
+    'depends': [
+        'purchase',
+    ],
+    'summary':"""""",
+    'description': """
+    """,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/res_country_views.xml',
+        'views/supplier_stock_views.xml',
+    ],
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
