1099119b368475e471130f4bc8effd7cb0928a99
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-01-17 12:23:01 +0800

    [FIX] cosmetics

--- supplier_stock/__openerp__.py
+++ supplier_stock/__openerp__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Rooms For (Hong Kong) Limted T/A OSCG
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 {
     'name': 'Supplier Stock',
     'category': 'Stock',
