92508d4b0cca6cf73fbe576994047a65dc475ab4
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-01-31 18:56:39 +0800

    [IMP] add stock_leadtime field

--- product_listprice_list_view/__openerp__.py
+++ product_listprice_list_view/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Product List Price Update List View',
-    'version': '8.0.1.1.1',
+    'version': '8.0.1.2.0',
     'author': 'Rooms For (Hong Kong) Ltd T/A OSCG',
     'website': 'https://www.odoo-asia.com',
     'category': 'Product',
