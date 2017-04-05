d1a444d59aacc516679169efa522536cfee4bff9
eHanseFrank <frank.schumacher@ehanse.de>
2017-04-05 19:14:59 +0800

    Adds images to suppler_stock, stock_move_extended and invoice_line

---
+++ stock_view_adjust_oaw/models/stock_move.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Rooms For (Hong Kong) Limted T/A OSCG
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields, api
+import openerp.addons.decimal_precision as dp
+
+
+class StockMoveLineExtended(models.Model):
+    _inherit = "stock.move"
+    image_small = fields.Binary(
+        'Image',
+        related='product_id.product_tmpl_id.image_small',
+    )
+
