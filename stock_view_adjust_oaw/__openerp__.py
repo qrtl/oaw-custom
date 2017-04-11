cf8f6b3ccb17afb3e7b9d55cf42fa919ce665a77
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-04-11 17:54:50 +0800

    Update product listprice listview (#57)
    * [ADD] web_tree_image
    
    * [IMP] make the view based on product.product, use qty_local_stock instead of qty_available
    
    * [IMP] remove qty_local_atp, adjust logic for local_stock
    
    * Adds images to suppler_stock, stock_move_extended and invoice_line
    
    * [IMP] add image_small, restructure files
    
    * [FIX] cosmetics
    
    * [IMP] add image_small, restructure files

--- stock_view_adjust_oaw/__openerp__.py
+++ stock_view_adjust_oaw/__openerp__.py
@@ -1,25 +1,12 @@
 # -*- coding: utf-8 -*-
-#    Odoo, Open Source Management Solution
-#    Copyright (C) 2015-2016 Rooms For (Hong Kong) Limited T/A OSCG
-#    <https://www.odoo-asia.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright 2015-2017 Rooms For (Hong Kong) Limted T/A OSCG
+# Copyright 2017 eHanse
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Stock View Adjust OAW',
     'summary': '',
-    'version': '8.0.0.7.0',
+    'version': '8.0.1.0.0',
     'category': 'Stock',
     'author': 'Rooms For (Hong Kong) Limited T/A OSCG',
     'website': 'https://www.odoo-asia.com',
@@ -30,11 +17,14 @@
     'installable': True,
     'auto_install': False,
     'images' : [],
-    'depends': ['sale_line_quant_extended',
-                'stock_picking_menu',
-                ],
+    'depends': [
+        'sale_line_quant_extended',
+        'stock_picking_menu',
+    ],
     'data': [
-         'stock_view.xml',        
+        'views/stock_move_views.xml',
+        'views/stock_picking_views.xml',
+        'views/stock_quant_views.xml',
     ],
     'test': [],
     'demo': [],
--- stock_view_adjust_oaw/models/__init__.py
+++ stock_view_adjust_oaw/models/__init__.py
@@ -3,4 +3,4 @@
 # Copyright 2017 eHanse
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import models
+from . import stock_move
