9035c9d0429f4555c5e0be4f6ba261cfac3bef24
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-04-11 17:28:46 +0800

    [IMP] add image_small, restructure files

--- stock_view_adjust_oaw/models/stock_move.py
+++ stock_view_adjust_oaw/models/stock_move.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Rooms For (Hong Kong) Limted T/A OSCG
+# Copyright 2015-2017 Rooms For (Hong Kong) Limted T/A OSCG
+# Copyright 2017 eHanse
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
-import openerp.addons.decimal_precision as dp
+from openerp import models, fields
 
 
-class StockMoveLineExtended(models.Model):
+class StockMove(models.Model):
     _inherit = "stock.move"
     image_small = fields.Binary(
         'Image',
         related='product_id.product_tmpl_id.image_small',
     )
-
