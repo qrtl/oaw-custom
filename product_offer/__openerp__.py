1af24bb909900b353e377079b802c08a5c80e275
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-04-21 13:57:38 +0800

    [IMP] minor cosmetic adjustments

--- product_offer/__openerp__.py
+++ product_offer/__openerp__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Product Offer',
-    'version': '8.0.1.2.3',
+    'version': '8.0.1.3.0',
     'author': 'Rooms For (Hong Kong) Limited T/A OSCG, eHanse',
     'website': 'https://www.odoo-asia.com',
     'category': 'Product',
