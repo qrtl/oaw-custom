66dd6ac0c174bc5fbd5c5e92df04cb2c92fe8df4
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-05-30 09:48:37 +0800

    update manifest files, cosmetics

--- product_listprice_list_view/__openerp__.py
+++ product_listprice_list_view/__openerp__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015-2017 Rooms For (Hong Kong) Limted T/A OSCG
+# Copyright 2015-2017 Quartile Limted
+# Copyright 2017 eHanse
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Product List Price Update List View',
-    'version': '8.0.1.3.1',
-    'author': 'Rooms For (Hong Kong) Ltd T/A OSCG',
+    'version': '8.0.1.4.0',
+    'author': 'Quartile Limited, eHanse',
     'website': 'https://www.odoo-asia.com',
     'category': 'Product',
     'depends': [
