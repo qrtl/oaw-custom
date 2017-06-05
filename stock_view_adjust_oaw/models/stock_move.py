6db325eab7bd3158f3c06026078eba72da8cc2fc
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-06-05 15:55:01 +0800

    Web tree image order line (#68)
    * [IMP] add small image to move lines under picking
    
    * cosmetics, company name change
    
    * [ADD] sale_view_adjust_oaw
    
    * [IMP] show image in PO lines, update module structure
    
    * cosmetics

--- stock_view_adjust_oaw/models/stock_move.py
+++ stock_view_adjust_oaw/models/stock_move.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015-2017 Rooms For (Hong Kong) Limted T/A OSCG
+# Copyright 2015-2017 Quartile Limted
 # Copyright 2017 eHanse
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -8,6 +8,7 @@ from openerp import models, fields
 
 class StockMove(models.Model):
     _inherit = "stock.move"
+
     image_small = fields.Binary(
         'Image',
         related='product_id.product_tmpl_id.image_small',
