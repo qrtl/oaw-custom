7bc35c6e17e85b85bb01292516f21523013da57f
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-06-26 17:31:57 +0800

    cosmetics, company name

--- sale_line_quant_extended/models/sale_order_line.py
+++ sale_line_quant_extended/models/sale_order_line.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Rooms For (Hong Kong) Limted T/A OSCG
+# Copyright 2015-2017 Quartile Limted
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, fields, api
@@ -72,6 +72,7 @@ class SaleOrderLine(models.Model):
         string="Purchase Order",
         related='purchase_line_id.order_id'
     )
+
 
     @api.multi
     def action_view_purchase_open(self):
