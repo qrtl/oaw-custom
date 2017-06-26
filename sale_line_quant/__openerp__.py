1aa64f72b34bf1ecc0cba569bd4e2a4f755acbbb
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-06-26 20:11:00 +0800

    costmetics, company name, etc.

--- sale_line_quant/__openerp__.py
+++ sale_line_quant/__openerp__.py
@@ -1,45 +1,31 @@
 # -*- coding: utf-8 -*-
-#    Odoo, Open Source Management Solution
-#    Copyright (C) 2015-2016 Rooms For (Hong Kong) Limited T/A OSCG
-#    <https://www.odoo-asia.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+# Copyright 2015-2017 Quartile Limted
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Quant/Serial Number on Sales',
     'category': 'Sale',
-    'version': '8.0.1.4.2',
-    'author': 'Rooms For (Hong Kong) Ltd. T/A OSCG',
-    'website': 'www.odoo-asia.com',
-    'depends': ['stock', 'sale_margin', 'vendor_consignment_stock',
-                'sale_owner_stock_sourcing',
-                'account_invoice_refund_link',
-                ],
+    'version': '8.0.1.5.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.odoo-asia.com',
+    'depends': [
+        'stock',
+        'sale_margin',
+        'vendor_consignment_stock',
+        'sale_owner_stock_sourcing',
+        'account_invoice_refund_link',
+    ],
     'summary':""" Serial Number Quant on Sales Order Line""",
     'description': """ 
 Modification on sales order line by adding quant and serial number selection.
     """,
     'data': [
-             'security/group.xml',
-             'views/sale_view.xml',
-             'views/purchase_view.xml',
-             'views/stock_view.xml',
-             'views/sale_stock_view.xml',
-             'views/so_line_quant_view.xml',
-             'views/account_invoice_view.xml',
-             ],
+        'security/group.xml',
+        'views/sale_view.xml',
+        'views/purchase_view.xml',
+        'views/stock_view.xml',
+        'views/sale_stock_view.xml',
+        'views/so_line_quant_view.xml',
+        'views/account_invoice_view.xml',
+    ],
     'installable': True,
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
