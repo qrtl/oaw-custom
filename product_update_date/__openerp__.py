1455c6f9a8f1eab748dfde9c18774aceed83c848
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-07-04 21:42:25 +0800

    [ADD] product_update_date

---
+++ product_update_date/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limted
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Product Update Date',
+    'version': '8.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.odoo-asia.com',
+    'category': 'Product',
+    'depends': [
+        "product_listprice_list_view",
+    ],
+    'description': """
+    """,
+    'data': [
+        'views/product_template_views.xml',
+    ],
+    'installable': True,
+}
---
+++ product_update_date/models/product_template.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limted
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields, api
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    updated_date = fields.Datetime(
+        compute='update_updated_date',
+        store=True,
+        string="Updated Date",
+    )
+
+
+    @api.multi
+    @api.depends('list_price', 'net_price', 'qty_reserved', 'qty_local_stock',
+                 'qty_overseas')
+    def update_updated_date(self):
+        for p in self:
+            p.updated_date = fields.Datetime.now()
