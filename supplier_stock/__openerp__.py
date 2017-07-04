e01028af916a60e1d6428bbfeef4e8f90b13c2b3
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-07-04 11:46:45 +0800

    Small changes on stock related views (#71)
    Small changes on stock related views

--- supplier_stock/__openerp__.py
+++ supplier_stock/__openerp__.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Rooms For (Hong Kong) Limted T/A OSCG
+# Copyright 2017 Quartile Limted
+# Copyright 2017 eHanse
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Supplier Stock',
     'category': 'Stock',
-    'version': '8.0.1.2.0',
-    'author': 'Rooms For (Hong Kong) Limited T/A OSCG',
+    'version': '8.0.1.2.1',
+    'author': 'Quartile Limited T/A OSCG',
     'website': 'https://www.odoo-asia.com',
     'depends': [
         'purchase',
@@ -20,4 +21,3 @@
     ],
     'installable': True,
 }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
