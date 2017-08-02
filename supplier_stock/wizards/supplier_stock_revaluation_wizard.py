96c9400656ae72ecdaa011568afe62a9d2bdfdab
tslai <tl@quartile.co>
2017-08-02 11:29:28 +0800

    [FIX] version increment, misc

--- supplier_stock/wizards/supplier_stock_revaluation_wizard.py
+++ supplier_stock/wizards/supplier_stock_revaluation_wizard.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Quartile Limited
+# Copyright 2017 eHanse
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import api, models, fields
