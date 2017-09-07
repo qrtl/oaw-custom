89a492a007a0ae75e9c77fdfd2bb593e8a9fde07
tslai <tl@quartile.co>
2017-09-07 09:27:50 +0000

    [IMP] Add customer refund cases and purchase own stock

--- profit_loss_report/models/profit_loss_report.py
+++ profit_loss_report/models/profit_loss_report.py
@@ -87,7 +87,10 @@ class ProfitLossReport(models.TransientModel):
     state = fields.Selection(
         [('purch_done', 'PO Done'),
          ('sale_done', 'SO Done'),
-         ('sale_purch_done', 'SO and PO Done')],
+         ('sale_purch_done', 'SO and PO Done'),
+         ('out_refund', 'Customer Refund'),
+         ('in_refund', 'Supplier Refund')
+        ],
         string='Status',
         readonly=True,
     )
@@ -215,7 +218,14 @@ class ProfitLossReport(models.TransientModel):
         string='Supplier Payment Status',
         readonly=True,
     )
-
+    invoice_type = fields.Selection(
+        [('out_invoice', 'Customer Invoice'),
+        ('in_invoice', 'Supplier Invoice'),
+        ('out_refund', 'Customer Refund'),
+        ('in_refund', 'Supplier Refund')],
+        string='Invoice Type',
+        readonly=True,
+    )
 
     @api.multi
     def _get_discount(self):
