b3a5c1837998780dc8a44db551cafe6522520535
Tim <tl@quartile.co>
2018-03-29 19:35:36 +0800

    Provide paid date and corresponding currency rate for account invoice (#114)

--- profit_loss_report/__openerp__.py
+++ profit_loss_report/__openerp__.py
@@ -3,11 +3,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Profit and Loss Report',
-    'version': '8.0.2.1.0',
+    'version': '8.0.3.0.0',
     'author': 'Quartile Limited',
     'website': 'https://www.quartile.co',
     'category': 'Report',
     'depends': [
+        'account_invoice_paid_date_currency',
         'sale_line_quant_extended',
         'product_offer',
     ],
