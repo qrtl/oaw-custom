1c12158488543583f7e17e16759481d1bc40f376
Tim <tl@quartile.co>
2018-04-04 20:38:53 +0800

    [FIX] Consignment Report access and supplier location (#117)

--- model_security_adjust_oaw/__openerp__.py
+++ model_security_adjust_oaw/__openerp__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 Quartile Limted
+# Copyright 2018 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Model Security Adjust OAW',
     'category': 'Security',
-    'version': '8.0.1.0.0',
+    'version': '8.0.1.0.1',
     'author': 'Quartile Limited',
     'website': 'https://www.quartile.co',
     'depends': [
