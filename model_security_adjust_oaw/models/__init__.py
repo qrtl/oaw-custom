64d73bbe693bfd04d5e94fbe35018e25d36c7e0e
tslai <tl@quartile.co>
2018-07-03 06:27:11 +0000

    [IMP] Add material and movement to product.template

--- model_security_adjust_oaw/models/__init__.py
+++ model_security_adjust_oaw/models/__init__.py
@@ -2,6 +2,8 @@
 
 from . import account_fiscalyear
 from . import account_period
-from . import supplier_stock
-from . import product_product
 from . import product_category
+from . import product_product
+from . import product_template
+from . import sale_order_line
+from . import supplier_stock
