8fb4ac8e13e0951c9b3bfbadd024fa77e8b9ec4a
tslai <tl@quartile.co>
2018-07-05 11:22:23 +0000

    [IMP] related_user in stock.move

--- model_security_adjust_oaw/models/__init__.py
+++ model_security_adjust_oaw/models/__init__.py
@@ -2,13 +2,12 @@
 
 from . import account_fiscalyear
 from . import account_period
-from . import supplier_stock
-from . import product_product
-from . import res_partner
-from . import sale_order_line
-from . import product_template
-from . import product_category
-from . import sale_order
 from . import account_voucher
-
-
+from . import product_category
+from . import product_product
+from . import product_template
+from . import res_partner
+from . import sale_order
+from . import sale_order_line
+from . import stock_move
+from . import supplier_stock
