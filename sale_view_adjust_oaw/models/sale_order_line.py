ce85d58f1247beca7da970e076f0b77bc4c7bd01
Tim <tl@quartile.co>
2018-07-13 15:51:17 +0800

    [IMP] Add line sequence field to sales order (#161)
    [FIX] Make sequence field readonly (#164)
    
    [IMP] Add Sequence to quotation report (#166)

--- sale_view_adjust_oaw/models/sale_order_line.py
+++ sale_view_adjust_oaw/models/sale_order_line.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Quartile Limted
+# Copyright 2017-2018 Quartile Limted
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, fields
@@ -17,5 +17,9 @@ class SaleOrderLine(models.Model):
     image_small = fields.Binary(
         'Image',
         related='product_id.product_tmpl_id.image_small',
+        readonly=True,
+    )
+    line_sequence = fields.Integer(
+        string="Sequence",
         readonly=True,
     )
