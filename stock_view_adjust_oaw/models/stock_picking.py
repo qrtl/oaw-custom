2bdec5b79a5d1b3892c63de56fb344938c675de2
Tim <tl@quartile.co>
2018-07-27 18:17:51 +0800

    Add "Sequence" to Delivery Order and Purchase Order (#167)
    * [IMP] Add sequence to purchase order
    * [IMP] Add sequence to stock_picking
    * [IMP] Adjust the css of the sales order form view

---
+++ stock_view_adjust_oaw/models/stock_picking.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Quartile Limted
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields, api
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    @api.multi
+    def write(self, vals):
+        res = super(StockPicking, self).write(vals)
+        for picking in self:
+            picking._update_move_line_sequence()
+        return res
+
+    @api.model
+    def create(self, vals):
+        res = super(StockPicking, self).create(vals)
+        for picking in res:
+            picking._update_move_line_sequence()
+        return res
+
+    def _update_move_line_sequence(self):
+        move_lines = sorted(self.move_lines, key=lambda r: (
+            r.line_sequence, r.id))
+        sequence = 1
+        for move_line in move_lines:
+            move_line.line_sequence = sequence
+            sequence += 1
