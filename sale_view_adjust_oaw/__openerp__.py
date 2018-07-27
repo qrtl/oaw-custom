bd5565cb671876a0d5df4bff701efe3e28f7cc7e
tslai <tl@quartile.co>
2018-07-27 10:14:18 +0000

    [IMP] Adjust the css of the sales order form view

--- sale_view_adjust_oaw/__openerp__.py
+++ sale_view_adjust_oaw/__openerp__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Sales View Adjust OAW',
     'summary': '',
-    'version': '8.0.1.1.0',
+    'version': '8.0.1.2.0',
     'category': 'Sales',
     'author': 'Quartile Limited',
     'website': 'https://www.quartile.co',
