4d491cdda3490ad53cce9f925781432661550b05
Tim <tl@quartile.co>
2018-09-20 16:31:27 +0800

    [IMP] Make filter case-insensitive, improve performance (#189)

--- model_security_adjust_oaw/wizards/profit_loss_report_wizard.py
+++ model_security_adjust_oaw/wizards/profit_loss_report_wizard.py
@@ -19,8 +19,9 @@ class ProfitLossReportWizard(models.TransientModel):
                                 "   SELECT id FROM res_partner WHERE "
                                 "   related_partner = %d)" %
                                 self.env.user.partner_id.id)
-            self._update_records()
+            self._update_supplier_info()
             self._filter_records()
+            self._update_records()
             self.env.cr.execute("UPDATE profit_loss_report SET "
                                 "state = NULL,"
                                 "supplier_id = NULL,"
