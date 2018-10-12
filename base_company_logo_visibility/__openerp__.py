0bcc9f0c0aedd87baf3ff5de721426d2dcdfaee9
Tim <tl@quartile.co>
2018-10-12 11:29:03 +0800

    [ADD] base_company_logo_visibility (#194)

---
+++ base_company_logo_visibility/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Company logo visibility',
+    'category': 'Security',
+    'version': '8.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.quartile.co',
+    'depends': [
+        'base',
+    ],
+    'summary':"""""",
+    'description': """
+    """,
+    'data': [
+        'security/res_security.xml',
+        'views/webclient_templates.xml',
+    ],
+    'installable': True,
+}
