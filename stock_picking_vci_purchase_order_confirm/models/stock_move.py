51133c85ccc768ab1fbe8096b03d8c3467182ce9
Tim <tl@quartile.co>
2018-10-24 15:44:28 +0800

    [ADD] stock_picking_vci_purchase_order_confirm (#196)

---
+++ stock_picking_vci_purchase_order_confirm/models/stock_move.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, fields, models
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    @api.multi
+    def action_assign(self):
+        for move in self:
+            if move.picking_id.sale_id:
+                move.picking_id.sale_id.confirm_vci_purhcase_order()
+        return super(StockMove, self).action_assign()
