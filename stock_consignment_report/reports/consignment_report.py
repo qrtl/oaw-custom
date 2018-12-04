39f0192c0c1a9c39d3834e9c010cdc300d3b6bd5
Tim <tl@quartile.co>
2018-12-04 15:39:05 +0800

    [IMP] Add reference field to the remark (#210)

--- stock_consignment_report/reports/consignment_report.py
+++ stock_consignment_report/reports/consignment_report.py
@@ -419,7 +419,10 @@ WHERE
                 quant.write({
                     'remark': quant.reservation_id.sudo().name_get()[0][1]})
             elif quant.sale_id:
-                quant.write({'remark': quant.sale_id.sudo().name})
+                remark = quant.sale_id.sudo().name
+                if quant.sale_id.sudo().client_order_ref:
+                    remark += " - " + quant.sale_id.sudo().client_order_ref
+                quant.write({'remark': remark})
 
     def _delete_supplier_loc_quant(self, model, section_id):
         quants = model.search([('section_id', '=', section_id)])
