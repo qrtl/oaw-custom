85963f8750d69d4fd5a5237b731923ef06e95ed7
tslai <tl@quartile.co>
2018-12-18 14:05:16 +0800

    [ADD] disable_json_xml_rpc

---
+++ disable_json_xml_rpc/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name' : 'Disable Json Xml RPC',
+    'version' : '8.0.1.0.0',
+    'author': 'Quartile Limited',
+    'category' : 'Security',
+    'description' : """
+Disable Json Xml RPC
+    """,
+    'website': 'https://www.quartile.co',
+    'depends' : [
+        'web'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
