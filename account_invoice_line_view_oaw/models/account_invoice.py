e1af48d747c8c67e290082324b3019adeb7c4860
Tim <tl@quartile.co>
2018-12-28 11:49:59 +0800

    [FIX] Improve formatting

--- account_invoice_line_view_oaw/models/account_invoice.py
+++ account_invoice_line_view_oaw/models/account_invoice.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017-2018 Quartile Limited
+# Copyright 2018 Quartile Limited
 # Copyright 2017 eHanse
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 
 from openerp import models, fields, api
 import openerp.addons.decimal_precision as dp
 
+
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
+    
     payment_ref= fields.Char(
         string='Payment Reference',
         compute = '_get_payment_reference',
     )
-
 
     @api.multi
     @api.depends('payment_ids')
@@ -25,3 +25,4 @@ class AccountInvoice(models.Model):
                     payment_ref.append(line.ref)
             invoice.payment_ref = ','.join(payment_ref) if len(
                 payment_ref) > 0 else False
+            
