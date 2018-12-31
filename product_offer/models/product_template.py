149054f74cf2ead112a0eefc348e2a6e7805e881
Frank Schumacher <eHanseFrank@users.noreply.github.com>
2018-12-31 16:09:05 +0800

    [IMP] Add currecy sales prices to product offer (#223)

--- product_offer/models/product_template.py
+++ product_offer/models/product_template.py
@@ -136,13 +136,78 @@ class ProductTemplate(models.Model):
         compute='_get_net_price_cny',
         digits=dp.get_precision('Product Price')
     )
-
-
     partner_stock_last_modified = fields.Datetime(
         string="Last Modified",
         readonly=True,
         store=True,
     )
+    sale_in_usd = fields.Float(
+        string='Sale USD',
+        compute='_get_sale_price_currency',
+        digits=dp.get_precision('Product Price')
+    )
+    sale_in_eur = fields.Float(
+        string='Sale EUR',
+        compute='_get_sale_price_currency',
+        digits=dp.get_precision('Product Price')
+    )
+    sale_in_chf = fields.Float(
+        string='Sale CHF',
+        compute='_get_sale_price_currency',
+        digits=dp.get_precision('Product Price')
+    )
+    sale_in_rmb = fields.Float(
+        string='Sale RMB',
+        compute='_get_sale_price_currency',
+        digits=dp.get_precision('Product Price')
+    )
+    sale_in_usd_so = fields.Float(
+        string='Sale USD',
+        compute='_get_sale_price_currency_discounted',
+        digits=dp.get_precision('Product Price')
+    )
+    sale_in_eur_so = fields.Float(
+        string='Sale EUR',
+        compute='_get_sale_price_currency_discounted',
+        digits=dp.get_precision('Product Price')
+    )
+    sale_in_chf_so = fields.Float(
+        string='Sale CHF',
+        compute='_get_sale_price_currency_discounted',
+        digits=dp.get_precision('Product Price')
+    )
+    sale_in_rmb_so = fields.Float(
+        string='Sale RMB',
+        compute='_get_sale_price_currency_discounted',
+        digits=dp.get_precision('Product Price')
+    )
+
+    @api.multi
+    def _get_sale_price_currency(self):
+        usd_rec = self.env['res.currency'].search([('name', '=', 'USD')])[0]
+        eur_rec = self.env['res.currency'].search([('name', '=', 'EUR')])[0]
+        chf_rec = self.env['res.currency'].search([('name', '=', 'CHF')])[0]
+        rmb_rec = self.env['res.currency'].search([('name', '=', 'CNY')])[0]
+        if usd_rec and eur_rec and chf_rec and rmb_rec:
+            for pt in self:
+                pt.sale_in_usd = pt.net_price * usd_rec.rate_silent
+                pt.sale_in_eur = pt.net_price * eur_rec.rate_silent
+                pt.sale_in_chf = pt.net_price * chf_rec.rate_silent
+                pt.sale_in_rmb = pt.net_price * rmb_rec.rate_silent
+
+    @api.multi
+    @api.depends('discount_aa_so')
+    def _get_sale_price_currency_discounted(self):
+        usd_rec = self.env['res.currency'].search([('name', '=', 'USD')])[0]
+        eur_rec = self.env['res.currency'].search([('name', '=', 'EUR')])[0]
+        chf_rec = self.env['res.currency'].search([('name', '=', 'CHF')])[0]
+        rmb_rec = self.env['res.currency'].search([('name', '=', 'CNY')])[0]
+        if usd_rec and eur_rec and chf_rec and rmb_rec:
+            for pt in self:
+                pt.sale_in_usd_so = pt.sale_hkd_aa_so * usd_rec.rate_silent
+                pt.sale_in_eur_so = pt.sale_hkd_aa_so * eur_rec.rate_silent
+                pt.sale_in_chf_so = pt.sale_hkd_aa_so * chf_rec.rate_silent
+                pt.sale_in_rmb_so = pt.sale_hkd_aa_so * rmb_rec.rate_silent
 
     @api.multi
     def _get_net_price_cny(self):
@@ -173,6 +238,7 @@ class ProductTemplate(models.Model):
             else:
                 pt.discount_aa_so = (1 - pt.sale_hkd_aa_so / pt.list_price) * 100
         return
+
     @api.multi
     @api.depends('list_price', 'sale_hkd_ac')
     def _get_discount_ac(self):
@@ -182,6 +248,7 @@ class ProductTemplate(models.Model):
             else:
                 pt.discount_ac = (1 - pt.sale_hkd_ac / pt.list_price) * 100
         return
+
     @api.multi
     @api.depends('list_price', 'sale_hkd_ac_so')
     def _get_discount_ac_so(self):
@@ -197,6 +264,7 @@ class ProductTemplate(models.Model):
     def _get_list_price_integer(self):
         for pt in self:
             pt.list_price_integer = int(pt.list_price)
+
     @api.multi
     @api.depends('net_price')
     def _get_net_price_integer(self):
@@ -211,11 +279,13 @@ class ProductTemplate(models.Model):
                 pt.local_stock = 'Yes'
             else:
                 pt.local_stock = 'No'
+
     @api.multi
     @api.depends('qty_local_stock', 'qty_reserved')
     def _get_local_stock_not_reserved(self):
         for pt in self:
             pt.local_stock_not_reserved = pt.qty_local_stock - pt.qty_reserved
+
     @api.multi
     @api.depends('qty_overseas')
     def _get_overseas_stock(self):
