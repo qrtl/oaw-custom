1491749efcc454ec2939ce5230f51373f4dd85be
Tim <tl@quartile.co>
2019-01-08 17:07:40 +0800

    [IMP] Capture the label by itertext() (#226)

--- product_export_kanban_image/wizard/export_product_image_wizard.py
+++ product_export_kanban_image/wizard/export_product_image_wizard.py
@@ -74,9 +74,7 @@ class ExportProductImageWizard(models.TransientModel):
             for node in etree.XML(fields['arch']).xpath("//li"):
                 if node.xpath(".//field") and not node.xpath(".//field")[
                     0].get("no_export", False):
-                    field_label = node.text or node.xpath("i") and \
-                                  node.xpath("i")[0].text or node.xpath("b")\
-                                  and node.xpath("b")[0].text
+                    field_label = "".join(node.itertext()).replace('%', '')
                     field_name = node.xpath(".//field")[0].get("name")
                     if ':' not in field_label:
                         field_label = [field_label, node.xpath(".//field")[
