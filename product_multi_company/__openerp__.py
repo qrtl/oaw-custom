118369151180cb7f66cdc8b699be6e1f690bce6a
tslai <tl@quartile.co>
2019-01-17 17:13:41 +0800

    [IMP] Update the record rules to restrict portal user

--- product_multi_company/__openerp__.py
+++ product_multi_company/__openerp__.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 Quartile Limited
+# Copyright 2018-2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Product Multi-Company Access Adjust',
     'category': 'Security',
-    'version': '8.0.1.0.0',
+    'version': '8.0.1.1.0',
     'author': 'Quartile Limited',
     'website': 'https://www.quartile.co',
     'depends': [
         'product',
+        'stock',
     ],
     'summary':"""""",
     'description': """
