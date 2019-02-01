a403ebcadf1a7ecf3cea676d06aaf37be1a9b014
tslai <tl@quartile.co>
2019-02-01 20:36:17 +0800

    [ADD] supplier_stock_hk_location

---
+++ supplier_stock_hk_location/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Supplier Location Ext',
+    'category': 'Stock',
+    'version': '8.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.quartile.co',
+    'depends': [
+        'supplier_stock',
+        'product_offer',
+    ],
+    'summary': """""",
+    'description': """
+    """,
+    'data': [
+        'views/supplier_location_views.xml',
+    ],
+    'installable': True,
+}
---
+++ supplier_stock_hk_location/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from . import product_product
+from . import stock_move
+from . import supplier_location
+from . import supplier_stock
