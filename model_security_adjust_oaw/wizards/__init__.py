aff4a264f48180cdcaad73f2836163fc7cae7f12
Tim <tl@quartile.co>
2019-02-25 15:26:37 +0800

    [IMP] Add customer reference to remark field for supplier's customer (#257)

--- model_security_adjust_oaw/wizards/__init__.py
+++ model_security_adjust_oaw/wizards/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from . import partner_statement_report_wizard
+from . import consignment_report_compute
 from . import consignment_report_wizard
 from . import profit_loss_report_wizard
