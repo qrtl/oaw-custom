a1bf8e8373a325a036d569edd8fa0a543569ef8c
Tim <tl@quartile.co>
2019-03-04 18:42:23 +0800

    [ADD] website_timecheck (#259)

---
+++ website_timecheck/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Website Timecheck Function',
+    'category': 'Website',
+    'version': '8.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.quartile.co',
+    'depends': [
+        'website_sale',
+        'website_sale_adj',
+        'website_multi_image_zoom',
+    ],
+    'summary':"""""",
+    'description': """
+    """,
+    'data': [
+        'security/timecheck_security.xml',
+        'views/templates.xml',
+    ],
+    'installable': True,
+}
