7d0543f6ef2d888463d19b2b313d33494739ed79
tslai <tl@quartile.co>
2019-03-28 18:23:18 +0800

    [ADD] sale_order_line_split

---
+++ sale_order_line_split/models/sale_order.py
@@ -0,0 +1,37 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields, api
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    need_split_line = fields.Boolean(
+        compute='_check_need_split_line',
+        store=True,
+        default=False,
+    )
+
+    @api.multi
+    def action_split_line(self):
+        for order in self:
+            for order_line in order.order_line:
+                if order_line.product_uom_qty > 1.0 and \
+                        order_line.product_id.tracking in ('serial', 'lot'):
+                    for qty in range(1, int(order_line.product_uom_qty)):
+                        order_line.copy(default={'product_uom_qty': 1.0,
+                                                 'order_id': order.id})
+                    order_line.product_uom_qty = 1.0
+        return True
+
+    @api.multi
+    @api.depends('order_line')
+    def _check_need_split_line(self):
+        for order in self:
+            for order_line in order.order_line:
+                if order_line.product_uom_qty > 1.0 and \
+                        order_line.product_id.tracking in ('serial', 'lot'):
+                    order.need_split_line = True
+                    print('Check Line')
+                    return
