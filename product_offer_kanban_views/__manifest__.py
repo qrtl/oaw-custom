7a6c5c347561ddf5b7af46e3af82c92aacf5e1f7
Tim <tl@quartile.co>
2019-04-10 18:40:35 +0800

    [ADD] product_offer_kanban_views (#292)

---
+++ product_offer_kanban_views/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Product Offer Kanban Views',
+    'version': '12.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.quartile.co',
+    'category': 'Product',
+    'depends': [
+        'product_local_oversea_stock_info',
+    ],
+    'description': """
+    """,
+    'data': [
+        'views/product_template_views.xml',
+    ],
+    'installable': True,
+}
