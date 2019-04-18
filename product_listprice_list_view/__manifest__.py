f3821efbab020295eda92ef659e3b62137bf8e56
Tim <tl@quartile.co>
2019-04-18 14:07:23 +0800

    [MIG] product_listprice_list_view (#300)

---
+++ product_listprice_list_view/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Product List Price Update List View',
+    'version': '12.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.quartile.co',
+    'category': 'Product',
+    'depends': [
+        "product_local_oversea_stock_info",
+        "product_offer_kanban_views",
+    ],
+    'description': """
+* Adds a menu item 'Product List Price Update' to facilitate list price update
+    """,
+    'data': [
+        'views/product_product_views.xml',
+    ],
+    'installable': True,
+}
