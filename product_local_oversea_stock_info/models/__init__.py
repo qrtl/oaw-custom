987ed04ba78a2a523befeb68a5fc8a4ead20cb94
tslai <tl@quartile.co>
2019-05-10 18:46:58 +0800

    [FIX] stock_location fields and _update_product_last_in_date

--- product_local_oversea_stock_info/models/__init__.py
+++ product_local_oversea_stock_info/models/__init__.py
@@ -4,6 +4,7 @@ from . import product_product
 from . import product_template
 from . import stock_quant
 from . import stock_move
+from . import stock_location
 from . import supplier_stock
 from . import sale_order
 from . import sale_order_line
