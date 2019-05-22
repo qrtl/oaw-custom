b1e12d6494d4401f0e506fc323c67b216b552370
Tim <tl@quartile.co>
2019-05-22 15:36:19 +0800

    [ADD] account_invoice_line_quant (#319)

---
+++ account_invoice_line_quant/models/account_invoice.py
@@ -0,0 +1,14 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields, api
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    # For Search
+    lot_id = fields.Many2one(
+        related='invoice_line_ids.lot_id',
+        string='Lot',
+    )
