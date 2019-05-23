1e4735e6c735aa3c0bc8ec1593800fa1e9609cd4
tslai <tl@quartile.co>
2019-05-23 18:53:02 +0800

    [MIG] sale_view_adjust_oaw

--- sale_view_adjust_oaw/__manifest__.py
+++ sale_view_adjust_oaw/__manifest__.py
@@ -1,24 +1,18 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017-2018 Quartile Limted
+# Copyright 2019 Quartile Limted
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Sales View Adjust OAW',
     'summary': '',
-    'version': '8.0.1.2.0',
+    'version': '12.0.1.0.0',
     'category': 'Sales',
     'author': 'Quartile Limited',
     'website': 'https://www.quartile.co',
     'description': """
     """,
     "license": "AGPL-3",
-    'application': False,
-    'installable': True,
-    'auto_install': False,
-    'images' : [],
     'depends': [
-        'sale',
-        'web_tree_image',
-        'sale_line_quant',
+        # 'web_tree_image',
+        'sale_order_line_quant',
         'sale_margin'
     ],
     'data': [
--- sale_view_adjust_oaw/models/sale_order_line.py
+++ sale_view_adjust_oaw/models/sale_order_line.py
@@ -2,7 +2,7 @@
 # Copyright 2017-2018 Quartile Limted
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class SaleOrderLine(models.Model):
