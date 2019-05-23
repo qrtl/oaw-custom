cffa6dd44fc01df1e4d39234fd9e9f5595b664d3
Tim <tl@quartile.co>
2019-05-23 16:32:38 +0800

    [ADD] res_partner_ext_oaw (#321)

---
+++ res_partner_ext_oaw/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Extension for partner',
+    'category': 'CRM',
+    'version': '12.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.quartile.co',
+    'depends': [
+        'base',
+    ],
+    'summary':"""""",
+    'description': """
+    """,
+    'data': [
+    ],
+    'installable': True,
+}
