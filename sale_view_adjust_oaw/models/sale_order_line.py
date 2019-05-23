98afdf3b38564266bfd1352aca6f8fdd2d1039b2
tslai <tl@quartile.co>
2019-05-23 18:54:35 +0800

    [FIX] Adjust comment

--- sale_view_adjust_oaw/models/sale_order_line.py
+++ sale_view_adjust_oaw/models/sale_order_line.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017-2018 Quartile Limted
+# Copyright 2019 Quartile Limted
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, fields
