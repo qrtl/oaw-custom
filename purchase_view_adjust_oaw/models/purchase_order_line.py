cfb3385c377327b370892419b3fe1aa84bf01295
Tim <tl@quartile.co>
2019-05-28 17:04:27 +0800

    [MIG] purchase_view_adjust_oaw (#323)

---
+++ purchase_view_adjust_oaw/models/purchase_order_line.py
@@ -0,0 +1,53 @@
+# Copyright 2019 Quartile Limted
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields, api
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+
+    image_small = fields.Binary(
+        'Image',
+        related='product_id.product_tmpl_id.image_small',
+        readonly=True,
+    )
+    line_sequence = fields.Integer(
+        string="Sequence",
+        readonly=True,
+    )
+    move_state = fields.Selection(
+        [('draft', 'New'),
+        ('cancel', 'Cancelled'),
+        ('confirmed', 'Waiting Availability'),
+        ('assigned', 'Available'),
+        ('done', 'Done'),
+        ('na', '(N/A)'),
+        ('multi', '(Multiple Statuses)'),
+        ],
+        compute='_get_move_state',
+        store=True,
+        readonly=True,
+        copy=False,
+        string='Move State',
+    )
+
+    @api.multi
+    @api.depends('move_ids.state')
+    def _get_move_state(self):
+        for line in self:
+            if line.move_ids:
+                if all(m.state == 'cancel' for m in line.move_ids):
+                    line.move_state = 'cancel'
+                else:
+                    state = ''
+                    for m in line.move_ids:
+                        if m.state == 'cancel':
+                            pass
+                        elif state == '':
+                            state = m.state
+                        elif state != m.state:
+                            state = 'multi'
+                    line.move_state = state
+            else:
+                line.move_state = 'na'
