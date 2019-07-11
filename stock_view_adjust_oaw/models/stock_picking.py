fc7eff8a2eec0f0998d780d2862876fa6c23a344
Tim <tl@quartile.co>
2019-07-11 17:57:56 +0800

    [MIG] stock_view_adjust_oaw (#343)

---
+++ stock_view_adjust_oaw/models/stock_picking.py
@@ -0,0 +1,30 @@
+# Copyright 2019 Quartile Limted
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields, api
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    @api.multi
+    def write(self, vals):
+        res = super(StockPicking, self).write(vals)
+        for picking in self:
+            picking._update_move_line_sequence()
+        return res
+
+    @api.model
+    def create(self, vals):
+        res = super(StockPicking, self).create(vals)
+        for picking in res:
+            picking._update_move_line_sequence()
+        return res
+
+    def _update_move_line_sequence(self):
+        move_lines = sorted(self.move_lines, key=lambda r: (
+            r.line_sequence, r.id))
+        sequence = 1
+        for move_line in move_lines:
+            move_line.line_sequence = sequence
+            sequence += 1
