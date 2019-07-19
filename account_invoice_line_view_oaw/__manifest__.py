8d6ff8f5c0a2d6c7350e5df630ccdc03c850e6dc
Tim <tl@quartile.co>
2019-07-19 18:55:57 +0800

    [MIG] account_invoice_line_view_oaw (#331)

---
+++ account_invoice_line_view_oaw/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright 2019 Quartile Limted
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Invoice Line View OAW',
+    'version': '12.0.1.0.0',
+    'category': 'Account',
+    'summary': 'Adds Invoice Line menu item',
+    'description': """
+Main Features
+==================================================
+* Add menu item Invoice Lines
+* Captures exchange rates as of the invoice dates and shows the base currency\
+ amounts in the output. 
+
+    """,
+    'author': 'Quartile Limited',
+    'website': 'http://www.quartile.co',
+    "license": "AGPL-3",
+    'depends': [
+        'account',
+        'sale_order_line_quant',
+        'account_invoice_line_quant',
+    ],
+    'data': [
+        'views/account_invoice_line_views.xml',
+    ],
+    'application': False,
+}
---
+++ account_invoice_line_view_oaw/models/account_invoice.py
@@ -0,0 +1,25 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api
+import odoo.addons.decimal_precision as dp
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    payment_ref = fields.Char(
+        string='Payment Reference',
+        compute='_get_payment_reference',
+    )
+
+    @api.multi
+    @api.depends('payment_ids')
+    def _get_payment_reference(self):
+        for invoice in self:
+            payment_ref = []
+            for line in invoice.payment_ids:
+                if line.payment_reference and line.payment_reference not in payment_ref:
+                    payment_ref.append(line.payment_reference)
+            invoice.payment_ref = ','.join(
+                payment_ref) if len(payment_ref) > 0 else False
