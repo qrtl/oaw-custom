803f022b1aee9b1ae9b9544859fb96e89b861efe
Tim <tl@quartile.co>
2019-07-30 17:56:19 +0800

    [MIG] account_invoice_refund_link (#350)

---
+++ account_invoice_refund_link/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Account Invoice Refund Link',
+    'category': 'Accounting & Finance',
+    'version': '12.0.1.0.0',
+    'website': 'https://www.quartile.co/',
+    'author': 'Quartile Limited',
+    'depends': [
+        'account'
+    ],
+    'license': 'AGPL-3',
+    'description': """
+    """,
+    'summary': '',
+    'data': [
+        'views/account_invoice_views.xml',
+    ],
+    'installable': True,
+}
---
+++ account_invoice_refund_link/models/account_invoice.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields, api
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.model
+    def _refund_cleanup_lines(self, lines):
+        for line in lines:
+            if not line.origin_invoice_line_id:
+                line.origin_invoice_line_id = line.id
+        res = super(AccountInvoice, self)._refund_cleanup_lines(lines)
+        for line in lines:
+            if line == line.origin_invoice_line_id:
+                line.origin_invoice_line_id = False
+        return res
