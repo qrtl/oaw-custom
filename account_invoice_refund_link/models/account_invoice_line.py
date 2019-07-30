9fe0361e88827f2c5c107db92d8f09f1b8633e10
tslai <tl@quartile.co>
2019-07-30 17:49:25 +0800

    [MIG] account_invoice_refund_link

---
+++ account_invoice_refund_link/models/account_invoice_line.py
@@ -0,0 +1,15 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields, api
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.invoice.line'
+
+    origin_invoice_line_id = fields.Many2one(
+        'account.invoice.line',
+        'Original Invoice Line',
+        copy=True,
+        readonly=True,
+    )
