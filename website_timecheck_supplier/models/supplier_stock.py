b2b8f5a49667254f0b0e409e15f2f92ff228e370
Tim <tl@quartile.co>
2019-08-08 16:34:09 +0800

    [IMP] Add separate page for supplier products (#358)

---
+++ website_timecheck_supplier/models/supplier_stock.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields, api
+
+
+class SupplierStock(models.Model):
+    _inherit = 'supplier.stock'
+
+    website_size_x = fields.Integer('Size X', default=1)
+    website_size_y = fields.Integer('Size Y', default=1)
+    website_style_ids = fields.Many2many('product.style', string='Styles')
+    name = fields.Char(
+        related='product_id.product_tmpl_id.name',
+        store=True
+    )
+    category_name = fields.Char(
+        related='product_id.product_tmpl_id.category_name',
+        store=True
+    )
+    default_code = fields.Char(
+        related='product_id.product_tmpl_id.default_code',
+        store=True,
+    )
+    custom_image = fields.Binary(string='Custom Image')
+
+    # Overwriting display_name's method for Supplier Access User
+    @api.multi
+    def name_get(self, *args, **kwargs):
+        result = []
+        for rec in self:
+            result.append(
+                (rec.id, rec.product_id.sudo().name)
+            )
+        return result
