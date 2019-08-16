d35a91ed5cba18816fbab20e61e5ed15916e38e0
tslai <tl@quartile.co>
2019-08-16 17:09:23 +0800

    [FIX] Avoid using user_id in website settings

--- sale_view_adjust_oaw/models/website.py
+++ sale_view_adjust_oaw/models/website.py
@@ -8,7 +8,7 @@ from openerp import models, fields, api
 class Website(models.Model):
     _inherit = 'website'
 
-    user_id = fields.Many2one(
+    sale_user_id = fields.Many2one(
         'res.users',
         string='Online Salesperson',
     )
