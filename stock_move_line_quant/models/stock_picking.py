2d59c448e0c2fd9246e615c9309519691ceef1a4
Tim <tl@quartile.co>
2019-09-26 14:03:01 +0800

    [FIX] Fix is_repair_location and sale_order_id, remove redundant files (#385)

--- stock_move_line_quant/models/stock_picking.py
+++ stock_move_line_quant/models/stock_picking.py
@@ -13,3 +13,16 @@ class StockPicking(models.Model):
         default=lambda self: self.env.user.company_id.currency_id,
         states={'draft': [('readonly', False)]},
     )
+    currency_id = fields.Many2one(
+        'res.currency',
+        string='Purchase Currency',
+    )
+    exchange_rate = fields.Float(
+        'FX Rate',
+        digits=(12, 6),
+    )
+
+    @api.onchange('currency_id')
+    def _onchange_currency_id(self):
+        if self.currency_id:
+            self.exchange_rate = self.currency_id.rate
