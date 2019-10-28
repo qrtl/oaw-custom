2ab9b405c090575d761037b319e933772858a2b6
tslai <tl@quartile.co>
2019-10-28 17:33:58 +0800

    [UPD] Module's description

--- website_sale_adj/__manifest__.py
+++ website_sale_adj/__manifest__.py
@@ -9,8 +9,9 @@
     'license': "AGPL-3",
     'description': """
 This module modify the website_sale module and provide following feature(s):
-1. Hide the Add to Cart button
-2. Add stock information to /shop page
+1. Add "About Us", "Contact Information" and "Empty Shop Display Message" to website settings
+2. Remove default design of footer, display "About Us", "Contact Information".
+3. Show "Empty Shop Display Message" when the shop page return empty result.
     """,
     'summary': "",
     'depends': [
