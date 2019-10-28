d8ddd6f7688514155b7a58c66f025e3ab8a4179d
Tim <tl@quartile.co>
2019-10-28 17:35:02 +0800

    [MIG] website_sale_adj (#398)

---
+++ website_sale_adj/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Website Sales Adjustment',
+    'version': '12.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.quartile.co',
+    'category': 'Website',
+    'license': "AGPL-3",
+    'description': """
+This module modify the website_sale module and provide following feature(s):
+1. Add "About Us", "Contact Information" and "Empty Shop Display Message" to website settings
+2. Remove default design of footer, display "About Us", "Contact Information".
+3. Show "Empty Shop Display Message" when the shop page return empty result.
+    """,
+    'summary': "",
+    'depends': [
+        'website_sale',
+    ],
+    'data': [
+        'views/res_config_settings_views.xml',
+        'views/templates.xml',
+    ],
+    'installable': True,
+}
---
+++ website_sale_adj/models/website_config_settings.py
@@ -0,0 +1,24 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields, api
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    about_us = fields.Text(
+        string='About Us',
+        related='website_id.about_us',
+        readonly=False,
+    )
+    contact_description = fields.Text(
+        string='Contact Information',
+        related='website_id.contact_description',
+        readonly=False,
+    )
+    empty_page_message = fields.Text(
+        string='Empty Shop Display Message',
+        related='website_id.empty_page_message',
+        readonly=False,
+    )
