012578887c11d90d77c4ec8d53f5275a4b32a907
tslai <tl@quartile.co>
2019-11-01 18:05:26 +0800

    [MIG] purchase_order_line_split

---
+++ purchase_order_line_split/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Split Purchase Order Lines',
+    'category': 'Purchase',
+    'summary': """""",
+    'version': '12.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.quartile.co',
+    'depends': [
+        'purchase_view_adjust_oaw',
+    ],
+    'description': """
+Adds a button in RFQ to split order lines so that each line has 1 for quantity.
+    """,
+    'data': [
+        'views/purchase_order_views.xml',
+    ],
+}
