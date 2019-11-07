a3491fd0be0151472e7194dbf14a117e1bb7c429
Frank Schumacher <eHanseFrank@users.noreply.github.com>
2019-11-07 13:54:50 +0800

    [MIG]  account_views_adj (#403)

---
+++ account_views_adj/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Quartile Limited, Timeware Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Account Views Adjustments',
+    'author': 'Timeware Ltd.',
+    'summary': '',
+    'version': '12.0.1.0.0',
+    'website': 'https://www.ehanse.de',
+    'category': 'Accounting',
+    'depends': [
+        "sale",
+        "account",
+    ],
+    'description': """
+- Adjust account payment views.
+    """,
+    'data': [
+        'views/account_payment_views.xml',
+    ],
+    'installable': True,
+}
