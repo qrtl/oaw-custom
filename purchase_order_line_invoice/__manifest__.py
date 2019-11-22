3888742a634ccb9d4eac80d45e5c36e68a9317ea
tslai <tl@quartile.co>
2019-11-22 15:04:49 +0800

    [ADD] purchase_order_line_invoice

---
+++ purchase_order_line_invoice/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Create Invoice From Purchase Order Lines',
+    'category': 'Purchase',
+    'summary': """""",
+    'version': '12.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.quartile.co',
+    'depends': [
+        'account',
+        'purchase_view_adjust_oaw',
+    ],
+    'description': """
+Add a view for invoicable purchase order line to create invoice.
+    """,
+    'data': [
+        'data/ir_actions.xml',
+        'views/purchase_order_line_views.xml',
+    ],
+}
