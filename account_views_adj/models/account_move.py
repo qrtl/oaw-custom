ad6b6ebac0e945532203e94b7b3d673c07d71ce6
eHanseFrank <frank.schumacher@ehanse.de>
2019-11-27 16:15:19 +0800

    Journal entry tree view editable.

---
+++ account_views_adj/models/account_move.py
@@ -0,0 +1,22 @@
+# Copyright 2019 Quartile Limited, Timeware Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models
+
+
+class AccountPayment(models.Model):
+    _inherit = "account.move"
+
+    @api.multi
+    def open_entry(self):
+        view_id = self.env.ref("account.view_move_form").id
+        return {
+            "name": "Journal Entry",
+            "view_mode": "form",
+            "view_type": "form",
+            "res_model": "account.move",
+            "view_id": view_id,
+            "res_id": self.id,
+            "target": "current",
+            "type": "ir.actions.act_window",
+        }
