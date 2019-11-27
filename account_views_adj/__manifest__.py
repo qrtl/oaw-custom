a1f9d8f6ba8be351580f008686d3b742da8186ea
Frank Schumacher <eHanseFrank@users.noreply.github.com>
2019-11-27 16:53:38 +0800

    [IMP] Make Entries tree view editable (#416)

--- account_views_adj/__manifest__.py
+++ account_views_adj/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Views Adjustments",
-    "author": "Timeware Ltd.",
+    "author": "Quartile Limited, Timeware Limted",
     "summary": "",
     "version": "12.0.1.0.0",
     "website": "https://www.ehanse.de",
@@ -11,6 +11,6 @@
     "description": """
 - Adjust account payment views.
     """,
-    "data": ["views/account_payment_views.xml"],
+    "data": ["views/account_payment_views.xml", "views/account_move_views.xml"],
     "installable": True,
 }
