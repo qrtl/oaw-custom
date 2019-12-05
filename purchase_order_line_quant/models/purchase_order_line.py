d7b3970bd0ee7dbecf5f93f1dc78077e26a78ace
Tim <tl@quartile.co>
2019-12-05 17:29:24 +0800

    [MIG] purchase_order_line_quant (#422)

---
+++ purchase_order_line_quant/models/purchase_order_line.py
@@ -0,0 +1,11 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, api, fields, models
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+
+    quant_id = fields.Many2one("stock.quant", string="Stock Quant", copy=False)
+    lot_id = fields.Many2one(related="quant_id.lot_id", string="Case No.")
