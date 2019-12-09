80d45e2fa70be214cb95689a8e84f28559660411
eHanseFrank <frank.schumacher@ehanse.de>
2019-12-09 16:52:40 +0800

    [IMP] black, isort

--- sale_view_adjust_oaw/__manifest__.py
+++ sale_view_adjust_oaw/__manifest__.py
@@ -14,7 +14,7 @@
         # 'web_tree_image',
         "sale_order_line_quant",
         "sale_margin",
-        'sale_management',
+        "sale_management",
     ],
     "data": ["data/ir_actions.xml", "views/sale_order_views.xml"],
 }
