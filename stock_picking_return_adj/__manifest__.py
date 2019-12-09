268d0bfc19432267c5ba5b4d59ad77e2daa08039
tslai <tl@quartile.co>
2019-12-09 17:20:25 +0800

    [ADD] stock_picking_return_adj

---
+++ stock_picking_return_adj/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Adjustments on Picking Return",
+    "version": "12.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "stock",
+    "description": """
+* Add "Is Returned" to indicate if the picking is being returned.
+* Hide the "Return" button when there is already a returned picking.
+* Only Inventory Manager can access the "Return" button and "Is Returned" field.
+    """,
+    "depends": ["stock"],
+    "data": ["views/stock_picking_views.xml"],
+    "installable": True,
+}
---
+++ stock_picking_return_adj/models/stock_picking.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    is_returned = fields.Boolean(
+        string='Is Returned',
+        groups='stock.group_stock_manager',
+        states={'done': [('readonly', False)]},
+        compute='_compute_is_returned',
+    )
+
+    @api.multi
+    def _compute_is_returned(self):
+        for picking in self:
+            picking.is_returned = True if picking.returned_ids else False
