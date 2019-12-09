3a609f0ea8fae66f5dce27143c12bb2368186240
tslai <tl@quartile.co>
2019-12-09 16:19:36 +0800

    [ADD] black, isort

--- purchase_order_line_quant/models/stock_rule.py
+++ purchase_order_line_quant/models/stock_rule.py
@@ -6,24 +6,29 @@ from odoo.exceptions import UserError
 
 
 class StockRule(models.Model):
-    _inherit = 'stock.rule'
+    _inherit = "stock.rule"
 
     @api.multi
-    def _run_buy(self, product_id, product_qty, product_uom, location_id, name, origin, values):
+    def _run_buy(
+        self, product_id, product_qty, product_uom, location_id, name, origin, values
+    ):
         cache = {}
-        if 'move_dest_ids' in values:
-            for move in values['move_dest_ids']:
+        if "move_dest_ids" in values:
+            for move in values["move_dest_ids"]:
                 if not move.quant_id.owner_id:
                     raise UserError(
-                        _('Owner is missing from from the %s (%s).' % (
-                            move.product_id.display_name,
-                            move.quant_id.display_name))
+                        _(
+                            "Owner is missing from from the %s (%s)."
+                            % (move.product_id.display_name, move.quant_id.display_name)
+                        )
                     )
-                values.update({
-                    'quant_id': move.quant_id.id,
-                    'price_unit': move.quant_id.purchase_price_unit,
-                    'currency_id': move.quant_id.currency_id.id,
-                })
+                values.update(
+                    {
+                        "quant_id": move.quant_id.id,
+                        "price_unit": move.quant_id.purchase_price_unit,
+                        "currency_id": move.quant_id.currency_id.id,
+                    }
+                )
                 order_line = move.sale_line_id
                 partner = move.quant_id.owner_id
         # <<< QTL Edit
@@ -43,26 +48,33 @@ class StockRule(models.Model):
         if domain in cache:
             po = cache[domain]
         else:
-            po = self.env['purchase.order'].sudo().search(
-                [dom for dom in domain])
+            po = self.env["purchase.order"].sudo().search([dom for dom in domain])
             po = po[0] if po else False
             cache[domain] = po
         if not po:
             vals = self._prepare_purchase_order(
-                product_id, product_qty, product_uom, origin, values, partner)
-            company_id = values.get(
-                'company_id') and values['company_id'].id or self.env.user.company_id.id
-            po = self.env['purchase.order'].with_context(
-                force_company=company_id).sudo().create(vals)
+                product_id, product_qty, product_uom, origin, values, partner
+            )
+            company_id = (
+                values.get("company_id")
+                and values["company_id"].id
+                or self.env.user.company_id.id
+            )
+            po = (
+                self.env["purchase.order"]
+                .with_context(force_company=company_id)
+                .sudo()
+                .create(vals)
+            )
             cache[domain] = po
-        elif not po.origin or origin not in po.origin.split(', '):
+        elif not po.origin or origin not in po.origin.split(", "):
             if po.origin:
                 if origin:
-                    po.write({'origin': po.origin + ', ' + origin})
+                    po.write({"origin": po.origin + ", " + origin})
                 else:
-                    po.write({'origin': po.origin})
+                    po.write({"origin": po.origin})
             else:
-                po.write({'origin': origin})
+                po.write({"origin": origin})
 
         # <<< QTL Edit
         # We do not merge PO lines
@@ -78,32 +90,35 @@ class StockRule(models.Model):
         # if not po_line:
         # >>> QTL Edit
         vals = self._prepare_purchase_order_line(
-            product_id, product_qty, product_uom, values, po, partner)
-        self.env['purchase.order.line'].sudo().create(vals)
+            product_id, product_qty, product_uom, values, po, partner
+        )
+        self.env["purchase.order.line"].sudo().create(vals)
         if order_line:
-            order_line.sudo().write({
-                'purchase_order_id': po.id
-            })
+            order_line.sudo().write({"purchase_order_id": po.id})
 
     @api.model
-    def _prepare_purchase_order_line(self, product_id, product_qty, product_uom, values, po, supplier):
+    def _prepare_purchase_order_line(
+        self, product_id, product_qty, product_uom, values, po, supplier
+    ):
         res = super(StockRule, self)._prepare_purchase_order_line(
-            product_id, product_qty, product_uom, values, po, supplier)
-        res['quant_id'] = values.get('quant_id', False)
-        res['price_unit'] = values.get('price_unit', False)
+            product_id, product_qty, product_uom, values, po, supplier
+        )
+        res["quant_id"] = values.get("quant_id", False)
+        res["price_unit"] = values.get("price_unit", False)
         return res
 
     def _make_po_get_domain(self, values, partner):
         domain = super(StockRule, self)._make_po_get_domain(values, partner)
-        if values.get('currency_id', False):
-            domain += (
-                ('currency_id', '=', values['currency_id']),
-            )
+        if values.get("currency_id", False):
+            domain += (("currency_id", "=", values["currency_id"]),)
         return domain
 
-    def _prepare_purchase_order(self, product_id, product_qty, product_uom, origin, values, partner):
+    def _prepare_purchase_order(
+        self, product_id, product_qty, product_uom, origin, values, partner
+    ):
         res = super(StockRule, self)._prepare_purchase_order(
-            product_id, product_qty, product_uom, origin, values, partner)
-        if values.get('currency_id', False):
-            res['currency_id'] = values['currency_id']
+            product_id, product_qty, product_uom, origin, values, partner
+        )
+        if values.get("currency_id", False):
+            res["currency_id"] = values["currency_id"]
         return res
