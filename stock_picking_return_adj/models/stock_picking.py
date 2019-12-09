4208e9db19e027e00feeb481f8beef3599750571
tslai <tl@quartile.co>
2019-12-09 18:45:01 +0800

    [ADD] black, isort

--- stock_picking_return_adj/models/stock_picking.py
+++ stock_picking_return_adj/models/stock_picking.py
@@ -5,18 +5,18 @@ from odoo import api, fields, models
 
 
 class StockPicking(models.Model):
-    _inherit = 'stock.picking'
+    _inherit = "stock.picking"
 
     is_returned = fields.Boolean(
-        string='Is Returned',
-        groups='stock.group_stock_manager',
-        states={'done': [('readonly', False)]},
-        compute='_compute_is_returned',
+        string="Is Returned",
+        groups="stock.group_stock_manager",
+        states={"done": [("readonly", False)]},
+        compute="_compute_is_returned",
         store=True,
     )
 
     @api.multi
-    @api.depends('move_lines.returned_move_ids')
+    @api.depends("move_lines.returned_move_ids")
     def _compute_is_returned(self):
         for picking in self:
             picking.is_returned = True if picking.returned_ids else False
