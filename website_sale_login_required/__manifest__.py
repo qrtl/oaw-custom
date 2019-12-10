c9745ee052048dfa32050510381ebbafce0bc474
Tim <tl@quartile.co>
2019-12-10 17:13:46 +0800

    [MIG] website_sale_login_required (#432)

---
+++ website_sale_login_required/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Online Shop Login Required",
+    "version": "12.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Website",
+    "license": "AGPL-3",
+    "description": """
+This module restricts the access of guest user to the following pages:
+1. Shop page
+2. Product page
+3. Shopping Cart page
+    """,
+    "summary": "",
+    "depends": ["website_sale"],
+    "data": [],
+    "installable": True,
+}
---
+++ website_sale_login_required/controllers/main.py
@@ -0,0 +1,40 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import http
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+
+class WebsiteSale(WebsiteSale):
+    @http.route(
+        [
+            """/shop""",
+            """/shop/page/<int:page>""",
+            """/shop/category/<model("product.public.category", "[('website_id', 'in', (False, current_website_id))]"):category>""",
+            """/shop/category/<model("product.public.category", "[('website_id', 'in', (False, current_website_id))]"):category>/page/<int:page>""",
+        ],
+        type="http",
+        auth="user",
+        website=True,
+    )
+    def shop(self, page=0, category=None, search="", ppg=False, **post):
+        return super(WebsiteSale, self).shop(
+            page=page, category=category, search=search, ppg=ppg, **post
+        )
+
+    @http.route(
+        ['/shop/product/<model("product.template"):product>'],
+        type="http",
+        auth="user",
+        website=True,
+    )
+    def product(self, product, category="", search="", **kwargs):
+        return super(WebsiteSale, self).product(
+            product=product, category=category, search=search, **kwargs
+        )
+
+    @http.route(["/shop/cart"], type="http", auth="user", website=True, sitemap=False)
+    def cart(self, access_token=None, revive="", **post):
+        return super(WebsiteSale, self).cart(
+            access_token=access_token, revive=revive, **post
+        )
