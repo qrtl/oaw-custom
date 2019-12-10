d51a1a272937d200f3d5bab7e1b16543014024f1
tslai <tl@quartile.co>
2019-12-10 18:36:42 +0800

    [ADD] black, isort

--- purchase_order_line_quant/models/account_invoice.py
+++ purchase_order_line_quant/models/account_invoice.py
@@ -8,9 +8,8 @@ class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
     def _prepare_invoice_line_from_po_line(self, line):
-        res = super(AccountInvoice,
-                    self)._prepare_invoice_line_from_po_line(line)
-        res['quantity'] = line.product_qty - line.qty_invoiced
+        res = super(AccountInvoice, self)._prepare_invoice_line_from_po_line(line)
+        res["quantity"] = line.product_qty - line.qty_invoiced
         if line and line.quant_id:
-            res['quant_id'] = line.quant_id.id
+            res["quant_id"] = line.quant_id.id
         return res
