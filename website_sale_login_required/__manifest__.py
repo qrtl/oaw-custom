febe5bde0ea8534295cbfd90d3981f81c639e6eb
tslai <tl@quartile.co>
2019-12-10 17:11:49 +0800

    [MIG] website_sale_login_required

--- website_sale_login_required/__manifest__.py
+++ website_sale_login_required/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Online Shop Login Required',
-    'version': '8.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Quartile Limited',
     'website': 'https://www.quartile.co',
     'category': 'Website',
--- website_sale_login_required/controllers/main.py
+++ website_sale_login_required/controllers/main.py
@@ -1,31 +1,29 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import http
-from openerp.addons.website_sale.controllers.main import website_sale
+from odoo import http
+from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
-class WebsiteSale(website_sale):
+class WebsiteSale(WebsiteSale):
 
     @http.route([
-        '/shop',
-        '/shop/page/<int:page>',
-        '/shop/category/<model("product.public.category"):category>',
-        '/shop/category/<model("product.public.category"):category>/page/<int:page>'
+        '''/shop''',
+        '''/shop/page/<int:page>''',
+        '''/shop/category/<model("product.public.category", "[('website_id', 'in', (False, current_website_id))]"):category>''',
+        '''/shop/category/<model("product.public.category", "[('website_id', 'in', (False, current_website_id))]"):category>/page/<int:page>'''
     ], type='http', auth="user", website=True)
-    def shop(self, page=0, category=None, search='', **post):
+    def shop(self, page=0, category=None, search='', ppg=False, **post):
         return super(WebsiteSale, self).shop(page=page, category=category,
-                                             search=search, **post)
+                                             search=search, ppg=ppg, **post)
 
-    @http.route(['/shop/product/<model("product.template"):product>'],
-                type='http', auth="user", website=True)
+    @http.route(['/shop/product/<model("product.template"):product>'], type='http', auth="user", website=True)
     def product(self, product, category='', search='', **kwargs):
         return super(WebsiteSale, self).product(product=product,
                                                 category=category,
                                                 search=search,
                                                 **kwargs)
 
-    @http.route(['/shop/cart'], type='http', auth="user", website=True)
-    def cart(self, **post):
-        return super(WebsiteSale, self).cart(**post)
+    @http.route(['/shop/cart'], type='http', auth="user", website=True, sitemap=False)
+    def cart(self, access_token=None, revive='', **post):
+        return super(WebsiteSale, self).cart(access_token=access_token, revive=revive, **post)
