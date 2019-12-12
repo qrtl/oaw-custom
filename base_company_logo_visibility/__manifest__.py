92ec257788c4700d5cf0001cb3d702c65080f623
Tim <tl@quartile.co>
2019-12-12 15:45:19 +0800

    [MIG] base_company_logo_visibility (#434)

---
+++ base_company_logo_visibility/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Company logo visibility",
+    "category": "Security",
+    "version": "12.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "depends": ["falcon_material_backend_theme"],
+    "summary": """""",
+    "description": """
+    """,
+    "data": [
+        "security/res_groups.xml",
+        "views/base_company_logo_visibility_templates.xml",
+    ],
+    "qweb": ["static/src/xml/new_menu.xml"],
+    "installable": True,
+}
