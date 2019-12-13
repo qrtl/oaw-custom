84045925559dbe5588f56b6d7355f16bc683e3e7
Tim <tl@quartile.co>
2019-12-13 17:20:38 +0800

    [MIG] stock_picking_vci_purchase_order_confirm (#435)

---
+++ stock_picking_vci_purchase_order_confirm/models/sale_order.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.multi
+    def confirm_vci_purhcase_order(self):
+        for order in self:
+            for order_line in order.order_line:
+                if (
+                    order_line.purchase_order_id
+                    and order_line.purchase_order_id.is_vci
+                    and order_line.purchase_order_id.state == "draft"
+                ):
+                    order_line.purchase_order_id.button_confirm()
+            order.picking_ids.mapped("move_lines")._recompute_state()
