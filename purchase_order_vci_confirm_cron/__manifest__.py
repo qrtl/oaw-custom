e7af4577003b8250eca6d8c27a9a4d928b00ef7d
Tim <tl@quartile.co>
2019-12-18 15:03:22 +0800

    [MIG] cron_purchase_confirm (#437)
    * [ADD] cron_purchase_confirm
    
    * [MIG] purchase_order_vci_confirm_cron
    
    * [ADD] black, isort

---
+++ purchase_order_vci_confirm_cron/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase Auto Confirm - VCI",
+    "category": "Purchases",
+    "version": "12.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "depends": ["vendor_consignment_stock"],
+    "description": """
+Purchase Order Confirm by Cron Job
+    """,
+    "license": "AGPL-3",
+    "summary": "",
+    "data": ["data/ir_cron_data.xml"],
+    "installable": True,
+}
