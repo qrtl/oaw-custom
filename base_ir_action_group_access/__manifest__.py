51da4abec40917aaf9d3dbe2dd7e1c6994e225cf
Tim <tl@quartile.co>
2019-12-18 16:07:34 +0800

    [MIG] group_action_access (#438)
    * [ADD] group_action_access (#216)
    
    * [MIG] group_action_access
    
    * [ADD] black, isort

---
+++ base_ir_action_group_access/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Restrict Action Access",
+    "version": "12.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Security",
+    "description": """
+Restrict action access base on the group settings of the action.
+    """,
+    "website": "https://www.quartile.co",
+    "depends": ["web"],
+    "installable": True,
+}
---
+++ base_ir_action_group_access/controllers/main.py
@@ -0,0 +1,30 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import _, http
+from odoo.addons.web.controllers.main import Action
+from odoo.exceptions import Warning
+from odoo.http import request
+
+
+class Action(Action):
+    @http.route("/web/action/load", type="json", auth="user")
+    def load(self, action_id, additional_context=None):
+        action_access = False
+        if action_id:
+            action = request.env["ir.actions.act_window"].search(
+                [("id", "=", int(action_id))]
+            )
+            if action and action.groups_id:
+                for group in action.groups_id.ids:
+                    if group in request.env.user.groups_id.ids:
+                        action_access = True
+                        break
+            else:
+                action_access = True
+        if not action_access:
+            raise Warning(_("Sorry, you are not allowed to access this document."))
+        else:
+            return super(Action, self).load(
+                action_id, additional_context=additional_context
+            )
