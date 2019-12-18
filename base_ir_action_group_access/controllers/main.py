0440e025ee8cf877021cde3fe414679c70e9d533
tslai <tl@quartile.co>
2019-12-18 16:05:03 +0800

    [MIG] group_action_access

--- base_ir_action_group_access/controllers/main.py
+++ base_ir_action_group_access/controllers/main.py
@@ -1,18 +1,16 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 Quartile Limited
+# Copyright 2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.exceptions import Warning
-from openerp.addons.web.controllers.main import Action
-from openerp import http
-from openerp.http import request
-from openerp.tools.translate import _
+from odoo.exceptions import Warning
+from odoo.addons.web.controllers.main import Action
+from odoo import http, _
+from odoo.http import request
 
 
 class Action(Action):
 
     @http.route('/web/action/load', type='json', auth="user")
-    def load(self, action_id, do_not_eval=False, additional_context=None):
+    def load(self, action_id, additional_context=None):
         action_access = False
         if action_id:
             action = request.env['ir.actions.act_window'].search([
@@ -26,9 +24,7 @@ class Action(Action):
             else:
                 action_access = True
         if not action_access:
-            raise Warning(_('Access Denied'),
-                          _("Sorry, you are not allowed to access this "
+            raise Warning(_("Sorry, you are not allowed to access this "
                             "document."))
         else:
-            return super(Action, self).load(action_id, do_not_eval=do_not_eval,
-                                            additional_context=additional_context)
+            return super(Action, self).load(action_id, additional_context=additional_context)
