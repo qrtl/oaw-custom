a45578bfce6d56ffa480ed0e134c15e0a66bad5d
tslai <tl@quartile.co>
2019-12-18 16:05:30 +0800

    [ADD] black, isort

--- base_ir_action_group_access/controllers/main.py
+++ base_ir_action_group_access/controllers/main.py
@@ -1,21 +1,20 @@
 # Copyright 2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.exceptions import Warning
+from odoo import _, http
 from odoo.addons.web.controllers.main import Action
-from odoo import http, _
+from odoo.exceptions import Warning
 from odoo.http import request
 
 
 class Action(Action):
-
-    @http.route('/web/action/load', type='json', auth="user")
+    @http.route("/web/action/load", type="json", auth="user")
     def load(self, action_id, additional_context=None):
         action_access = False
         if action_id:
-            action = request.env['ir.actions.act_window'].search([
-                ('id', '=', int(action_id))
-            ])
+            action = request.env["ir.actions.act_window"].search(
+                [("id", "=", int(action_id))]
+            )
             if action and action.groups_id:
                 for group in action.groups_id.ids:
                     if group in request.env.user.groups_id.ids:
@@ -24,7 +23,8 @@ class Action(Action):
             else:
                 action_access = True
         if not action_access:
-            raise Warning(_("Sorry, you are not allowed to access this "
-                            "document."))
+            raise Warning(_("Sorry, you are not allowed to access this " "document."))
         else:
-            return super(Action, self).load(action_id, additional_context=additional_context)
+            return super(Action, self).load(
+                action_id, additional_context=additional_context
+            )
