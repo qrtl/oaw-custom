dbc6ce2e3e99f52c5754a33ac67b207e3738798d
Tim <tl@quartile.co>
2020-01-14 16:21:00 +0800

    [FIX] Advance search javascript (#446)

--- supplier_user_access/static/src/js/enable_advance_search.js
+++ supplier_user_access/static/src/js/enable_advance_search.js
@@ -1,19 +1,27 @@
 odoo.define('supplier_user_access.EnableAdvanceSearch', function (require) {
     "use strict";
 
-    var SearchView = require('web.SearchView');
+    var AbstractController = require('web.AbstractController');
 
-    var EnableAdvanceSearch = SearchView.extend({
-        start: function () {
-            this._super.apply(this, arguments);
-            debugger;
-            var self = this;
+    var EnableAdvanceSearch = AbstractController.include({
+        _update: function (state) {
+            var def = this._super(state);
             this.getSession().user_has_group('stock.group_stock_user').then(function (has_group) {
                 if (has_group) {
-                    $('o_searchview_more fa').show();
+                    $('div.o_cp_right').show();
+                    $('div.o_mobile_search_filter').show();
+                    $('span.o_searchview_more').show();
+                }
+                else {
+                    $('div.o_search_options').hide();
+                    $('div.o_mobile_search_filter').hide();
+                    $('span.o_searchview_more').hide();
+                    $('div.o_cp_right').show();
                 }
             });
+            return def;
         },
     });
+
     return EnableAdvanceSearch;
 });
