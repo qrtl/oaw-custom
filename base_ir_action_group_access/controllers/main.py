ba1839e7f0ac1c3a4a0c64116bf8c1ed9b41794b
tslai <tl@quartile.co>
2020-01-14 16:17:30 +0800

    [FIX] Check action_id in load()

--- base_ir_action_group_access/controllers/main.py
+++ base_ir_action_group_access/controllers/main.py
@@ -11,6 +11,15 @@ class Action(Action):
     @http.route("/web/action/load", type="json", auth="user")
     def load(self, action_id, additional_context=None):
         action_access = False
+        try:
+            action_id = int(action_id)
+        except ValueError:
+            try:
+                action = request.env.ref(action_id)
+                assert action._name.startswith('ir.actions.')
+                action_id = action.id
+            except Exception:
+                action_id = 0   # force failed read
         if action_id:
             action = request.env["ir.actions.act_window"].search(
                 [("id", "=", int(action_id))]
@@ -23,7 +32,8 @@ class Action(Action):
             else:
                 action_access = True
         if not action_access:
-            raise Warning(_("Sorry, you are not allowed to access this document."))
+            raise Warning(
+                _("Sorry, you are not allowed to access this document."))
         else:
             return super(Action, self).load(
                 action_id, additional_context=additional_context
