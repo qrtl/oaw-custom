6d97b6408f097d6129de2a4afa54f5fc9ea47e71
Tim <tl@quartile.co>
2020-01-22 19:03:19 +0800

    Update __manifest__.py

--- base_users_list_views/__manifest__.py
+++ base_users_list_views/__manifest__.py
@@ -1,9 +1,8 @@
-# Copyright 2015-2018 Quartile Limted
-# Copyright 2017 eHanse
+# Copyright 2020 Quartile Limited, Timeware Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "User List Views",
-    "version": "12.0.0.1",
+    "name": "External User List Views",
+    "version": "12.0.1.0.1",
     "author": "Timeware Limited, Quartile Limited",
     "website": "",
     "category": "",
