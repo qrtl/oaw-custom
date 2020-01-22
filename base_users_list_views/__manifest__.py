6337487b0616c9743e7fd3eccdf5a1c6aa106d2d
Frank Schumacher <eHanseFrank@users.noreply.github.com>
2020-01-22 19:03:47 +0800

    [ADD] base_users_list_views (#453)

---
+++ base_users_list_views/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Quartile Limited, Timeware Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "External User List Views",
+    "version": "12.0.1.0.1",
+    "author": "Timeware Limited, Quartile Limited",
+    "website": "",
+    "category": "",
+    "depends": ["supplier_user_access"],
+    "description": """
+* Adds further tree views for user belonging to "Supplier" or "Timecheck"
+    """,
+    "data": ["views/view_user_tree.xml"],
+    "installable": True,
+}
