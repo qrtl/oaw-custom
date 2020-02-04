00b13f2fadf8482c7e0ee7a585454909a1573242
eHanseFrank <frank.schumacher@ehanse.de>
2020-02-04 14:54:53 +0800

    Migration of oa_order-line_views

---
+++ sale_mto_order_lines/models/sale_order_line.py
@@ -0,0 +1,28 @@
+# Copyright 2020 Quartile Limited, Timeware Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    prod_ref = fields.Char(
+        related="product_id.product_tmpl_id.name",
+        string="Product Reference",
+        store=True,
+    )
+    prod_code = fields.Char(
+        related="product_id.product_tmpl_id.default_code", string="Code", store=True
+    )
+    is_mto = fields.Boolean(string="MTO", related="order_id.is_mto")
+    order_price_list = fields.Char(
+        related="order_id.pricelist_id.name", string="Price List"
+    )
+    order_date = fields.Datetime(related="order_id.date_order", string="Date")
+    supplier_code = fields.Char(
+        "Supplier Code", related="order_id.supplier_code", store=True
+    )
+    supplier_note = fields.Char("Supplier Notes")
+    sales_remark = fields.Char("Sales Team Remark")
