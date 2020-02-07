83aec890dd978b4d7b65a8e51fd96e6ad154947e
tslai <tl@quartile.co>
2020-02-07 15:13:14 +0800

    [ADD] black, isort

--- stock_view_adjust_oaw/models/stock_move_line.py
+++ stock_view_adjust_oaw/models/stock_move_line.py
@@ -10,32 +10,12 @@ class StockMoveLine(models.Model):
     image_medium = fields.Binary(
         "Image", related="product_id.product_tmpl_id.image_medium", readonly=True
     )
-    move_name = fields.Char(
-        related="move_id.name",
-        store=True,
-    )
-    origin = fields.Char(
-        related="move_id.origin",
-        store=True,
-    )
-    sale_order_id = fields.Many2one(
-        related="move_id.sale_line_id.order_id",
-    )
-    purchase_order_id = fields.Many2one(
-        related="move_id.purchase_line_id.order_id",
-    )
-    group_id = fields.Many2one(
-        related="move_id.group_id",
-    )
-    partner_id = fields.Many2one(
-        related='move_id.partner_id',
-    )
-    picking_partner_id = fields.Many2one(
-        related='picking_id.partner_id',
-    )
-    picking_type_id = fields.Many2one(
-        related='move_id.picking_type_id',
-    )
-    date_expected = fields.Datetime(
-        related='move_id.date_expected',
-    )
+    move_name = fields.Char(related="move_id.name", store=True)
+    origin = fields.Char(related="move_id.origin", store=True)
+    sale_order_id = fields.Many2one(related="move_id.sale_line_id.order_id")
+    purchase_order_id = fields.Many2one(related="move_id.purchase_line_id.order_id")
+    group_id = fields.Many2one(related="move_id.group_id")
+    partner_id = fields.Many2one(related="move_id.partner_id")
+    picking_partner_id = fields.Many2one(related="picking_id.partner_id")
+    picking_type_id = fields.Many2one(related="move_id.picking_type_id")
+    date_expected = fields.Datetime(related="move_id.date_expected")
