95660a8d7a38e0f2c2b580cd9f7a4420e8855d2d
Tim <tl@quartile.co>
2020-02-11 14:49:35 +0800

    [ADD] sale_order_deliver_status (#475)
    * [ADD] sale_order_deliver_status
    
    * [ADD] black, isort
    
    * Update sale_order_views.xml

---
+++ sale_order_deliver_status/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Quartile Limted
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sales Order Deliver Status",
+    "summary": "",
+    "version": "12.0.1.0.0",
+    "category": "Sales",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "description": """
+    """,
+    "license": "AGPL-3",
+    "depends": ["sale_management"],
+    "data": ["views/sale_order_views.xml"],
+}
