c152efb5c6b51e968d4c4b8b82d89e85ac7d4843
tslai <tl@quartile.co>
2020-02-14 23:38:36 +0800

    [ADD] black, isort

--- stock_quant_custom_transfer/wizard/stock_quant_transfer.py
+++ stock_quant_custom_transfer/wizard/stock_quant_transfer.py
@@ -9,16 +9,12 @@ class QuantTransferWizard(models.TransientModel):
     _name = "quant.transfer.wizard"
 
     location_dest_id = fields.Many2one(
-        "stock.location",
-        string="Destination Location",
-        required=True,
+        "stock.location", string="Destination Location", required=True
     )
     picking_type_id = fields.Many2one(
         "stock.picking.type", string="Operation Type", required=True
     )
-    sale_order_id = fields.Many2one(
-        "sale.order", string="Related Sales Order"
-    )
+    sale_order_id = fields.Many2one("sale.order", string="Related Sales Order")
 
     @api.onchange("picking_type_id")
     def onchange_picking_type(self):
@@ -32,9 +28,7 @@ class QuantTransferWizard(models.TransientModel):
                 ]._get_partner_locations()[0]
             if self.picking_type_id.code == "outgoing":
                 location_domain = [("usage", "=", ("customer", "supplier"))]
-        return {
-            'domain': {'location_dest_id': location_domain}
-        }
+        return {"domain": {"location_dest_id": location_domain}}
 
     @api.multi
     def action_stock_quant_transfer(self):
@@ -50,8 +44,7 @@ class QuantTransferWizard(models.TransientModel):
         #  if selected quants are from different location then raise to
         #  avoid confusion of taking source location for picking
         if any(q.location_id != source_location for q in quant_ids):
-            raise UserError(
-                _("Please select quants that are in the same " "Location."))
+            raise UserError(_("Please select quants that are in the same " "Location."))
 
         if source_location == self.location_dest_id:
             raise UserError(_("Please select different location to transfer."))
@@ -83,7 +76,11 @@ class QuantTransferWizard(models.TransientModel):
                 "lot_id": quant.lot_id.id,
             }
             if self.sale_order_id:
-                line_vals["group_id"] = self.sale_order_id.procurement_group_id and self.sale_order_id.procurement_group_id.id or False
+                line_vals["group_id"] = (
+                    self.sale_order_id.procurement_group_id
+                    and self.sale_order_id.procurement_group_id.id
+                    or False
+                )
             new_move = stock_move_obj.new(line_vals)
             new_move.onchange_product_id()
             move_dict = stock_move_obj._convert_to_write(
