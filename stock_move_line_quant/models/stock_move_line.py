f68939039792e9cab744ece6dcbf8aa6c337eb3f
tslai <tl@quartile.co>
2020-02-14 23:26:38 +0800

    [FIX] Avoid update resvered quantity

--- stock_move_line_quant/models/stock_move_line.py
+++ stock_move_line_quant/models/stock_move_line.py
@@ -31,7 +31,8 @@ class StockMoveLine(models.Model):
         related="move_id.picking_type_id.code", string="Type of Operation", store=True
     )
     quant_id = fields.Many2one("stock.quant", string="Stock Quant")
-    quant_owner_id = fields.Many2one(related="quant_id.owner_id", string="Quant Owner")
+    quant_owner_id = fields.Many2one(
+        related="quant_id.owner_id", string="Quant Owner")
 
     @api.onchange("quant_id")
     def _onchange_quant_id(self):
@@ -74,12 +75,14 @@ class StockMoveLine(models.Model):
                     vals["owner_id"] = picking.owner_id.id
         if "lot_id" in vals and vals["lot_id"]:
             vals["quant_id"] = (
-                self.env["stock.production.lot"].browse(vals["lot_id"]).quant_ids[0].id
+                self.env["stock.production.lot"].browse(
+                    vals["lot_id"]).quant_ids[0].id
             )
         res = super(StockMoveLine, self).create(vals)
         # Update the reservation_id of the stock quant
         if res.lot_id and res.move_id:
-            res.lot_id.quant_ids.sudo().update({"reservation_id": res.move_id.id})
+            res.lot_id.quant_ids.sudo().update(
+                {"reservation_id": res.move_id.id})
         return res
 
     @api.multi
@@ -123,6 +126,6 @@ class StockMoveLine(models.Model):
         for move_line in self:
             if move_line.lot_id:
                 move_line.lot_id.quant_ids.sudo().update(
-                    {"reservation_id": False, "reserved_quantity": 0}
+                    {"reservation_id": False}
                 )
         return super(StockMoveLine, self).unlink()
