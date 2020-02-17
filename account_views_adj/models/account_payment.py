97009e104d790a6ce687e7bc113eb1165380c24f
tslai <tl@quartile.co>
2020-02-17 16:57:33 +0800

    [ADD] black, isort

--- account_views_adj/models/account_payment.py
+++ account_views_adj/models/account_payment.py
@@ -10,25 +10,20 @@ class AccountPayment(models.Model):
     sale_order_ids = fields.Many2many(
         "sale.order", compute="_get_sale_order_ref", store=True, search="_search_by_so"
     )
-    sale_order_id = fields.Char(
-        "Sale Order", related="sale_order_ids.display_name")
+    sale_order_id = fields.Char("Sale Order", related="sale_order_ids.display_name")
     payment_info = fields.Char("Payment Info")
     payment_reviewed = fields.Boolean("Checked")
-    invoice_type = fields.Selection(
-        related="invoice_ids.type",
-    )
-    inverse_amount = fields.Monetary(
-        string='Payment Amount',
-    )
+    invoice_type = fields.Selection(related="invoice_ids.type")
+    inverse_amount = fields.Monetary(string="Payment Amount")
 
     @api.model
     def default_get(self, fields):
         rec = super(AccountPayment, self).default_get(fields)
-        if 'amount' in rec and rec['amount']:
-            rec['inverse_amount'] = -rec['amount']
+        if "amount" in rec and rec["amount"]:
+            rec["inverse_amount"] = -rec["amount"]
         return rec
 
-    @api.onchange('inverse_amount')
+    @api.onchange("inverse_amount")
     def _onchange_payment_amount(self):
         if self.inverse_amount:
             self.amount = -self.inverse_amount
