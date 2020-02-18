cc010bec47fb2213c434c6b3bb7a3d2f18f9ff20
Frank Schumacher <eHanseFrank@users.noreply.github.com>
2020-02-18 16:30:10 +0800

    12.0 mig oa order line views vi (#484)
    * Removed commented lines
    
    * [IMP]black, isort
    
    * Update sale_order_views.xml
    
    Co-authored-by: Tim <tl@quartile.co>

---
+++ sale_mto_order_lines/models/sale_order.py
@@ -0,0 +1,48 @@
+# Copyright 2020 Quartile Limited, Timeware Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    supplier_id = fields.Many2one(
+        comodel_name="res.partner", string="Sales Supplier", store=True
+    )
+    supplier_code = fields.Char("Code", related="supplier_id.ref", store=True)
+    is_shipment = fields.Boolean("Shipment")
+
+    @api.model
+    def create(self, vals):
+        if vals["is_mto"]:
+            user = self.env.user
+            # If Supplier creates a MTO, it will be declared as is_shipment
+            if user.has_group("supplier_user_access.group_supplier"):
+                vals["is_shipment"] = True
+            # If internal user creates MTO, user will required to set a Sales Supplier
+            if not user.has_group("supplier_user_access.group_supplier"):
+                if "supplier_id" in vals:
+                    if not vals["supplier_id"]:
+                        raise UserError("For MTO a Sales Supplier has to be selected!")
+        res = super(SaleOrder, self).create(vals)
+        self.sudo().message_subscribe([self.supplier_id])
+
+    @api.multi
+    def write(self, vals):
+        for rec in self:
+            if rec.is_mto:
+                user = self.env.user
+                # If Supplier creates a MTO, it will be declared as is_shipment
+                if user.has_group("supplier_user_access.group_supplier"):
+                    vals["is_shipment"] = True
+                # If internal user creates MTO, user will required to set a Sales Supplier
+                if not user.has_group("supplier_user_access.group_supplier"):
+                    if "supplier_id" in vals:
+                        if not vals["supplier_id"]:
+                            raise UserError(
+                                "For MTO a Sales Supplier has to be selected!"
+                            )
+        res = super(SaleOrder, self).write(vals)
+        return res
