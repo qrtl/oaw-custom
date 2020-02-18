fea5bbcd8b31c570828fde2a8cfbacf0b9ac4f46
eHanseFrank <frank.schumacher@ehanse.de>
2020-02-18 04:11:31 +0000

    [IMP]black, isort

--- quotation_report_adjust/models/sale_order.py
+++ quotation_report_adjust/models/sale_order.py
@@ -7,32 +7,25 @@ from odoo import api, fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    order_ref_report = fields.Char(
-        string="Code",
-        store=True,
+    order_ref_report = fields.Char(string="Code", store=True)
 
-    )
+    quot_report_note = fields.Text("Notes")
 
-    quot_report_note = fields.Text(
-        'Notes'
-    )
     @api.model
     def create(self, vals):
         res = super(SaleOrder, self).create(vals)
         # For quotation adjust: set new order_ref field
-        if 'name' in vals and 'partner_id' in vals:
-              name = vals['name']
-              #Get the reference number number
-              fragments_order_ref = vals['name'].split("-")
-              sub_order_ref = fragments_order_ref[-1]
-              #Get the partner id
-              domain = [
-                  ('id', '=', vals['partner_id']),
-              ]
-              partner_id = self.env['res.partner'].search(domain)
-              partner_ref = partner_id.ref
-              # New reference
-              if partner_ref == False:
-                  partner_ref = partner_id.name
-              res.order_ref_report = sub_order_ref+" "+partner_ref
+        if "name" in vals and "partner_id" in vals:
+            name = vals["name"]
+            # Get the reference number number
+            fragments_order_ref = vals["name"].split("-")
+            sub_order_ref = fragments_order_ref[-1]
+            # Get the partner id
+            domain = [("id", "=", vals["partner_id"])]
+            partner_id = self.env["res.partner"].search(domain)
+            partner_ref = partner_id.ref
+            # New reference
+            if partner_ref == False:
+                partner_ref = partner_id.name
+            res.order_ref_report = sub_order_ref + " " + partner_ref
         return res
