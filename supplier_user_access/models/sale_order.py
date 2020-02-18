6af7a1199951b54fb644597abb6f52cb28e7faee
tslai <tl@quartile.co>
2020-02-18 17:52:26 +0800

    [FIX] Remove automated action part

--- supplier_user_access/models/sale_order.py
+++ supplier_user_access/models/sale_order.py
@@ -21,27 +21,6 @@ class SaleOrder(models.Model):
             "supplier_user_access.report_sale_supplier_fm"
         ).report_action(self)
 
-    @api.multi
-    def write(self, vals):
-        res = super(SaleOrder, self).write(vals)
-        for so in self:
-            if self.env.user.has_group("supplier_user_access.group_supplier"):
-                server_actions = (
-                    self.env["base.action.rule"]
-                    .sudo()
-                    .search(
-                        [
-                            ("model", "=", "sale.order"),
-                            ("kind", "in", ("on_write", "on_create_or_write")),
-                            ("active", "=", True),
-                        ],
-                        order="sequence",
-                    )
-                )
-                for action in server_actions:
-                    action.sudo()._process(action, [so.id])
-        return res
-
     @api.model
     def create(self, vals):
         if (
@@ -58,21 +37,6 @@ class SaleOrder(models.Model):
             fragments_order_ref = vals["name"].split("-")
             sub_order_ref = fragments_order_ref[-1]
             res.order_ref_fm_report = sub_order_ref
-        if self.env.user.has_group("supplier_user_access.group_supplier"):
-            server_actions = (
-                self.env["base.action.rule"]
-                .sudo()
-                .search(
-                    [
-                        ("model", "=", "sale.order"),
-                        ("kind", "in", ("on_create", "on_create_or_write")),
-                        ("active", "=", True),
-                    ],
-                    order="sequence",
-                )
-            )
-            for action in server_actions:
-                action.sudo()._process(action, [res.id])
         return res
 
     def action_supplier_view_delivery(self):
@@ -85,9 +49,11 @@ class SaleOrder(models.Model):
 
         # choose the view_mode accordingly
         if len(picking_ids) > 1:
-            result.domain = "[('id','in',[" + ",".join(map(str, picking_ids)) + "])]"
+            result.domain = "[('id','in',[" + \
+                ",".join(map(str, picking_ids)) + "])]"
         else:
-            form_view = self.env.ref("supplier_user_access.view_supplier_picking_form")
+            form_view = self.env.ref(
+                "supplier_user_access.view_supplier_picking_form")
             result.views = [(form_view and form_view[1] or False, "form")]
             result.res_id = picking_ids and picking_ids[0] or False
 
