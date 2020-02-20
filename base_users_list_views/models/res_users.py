b54ece0b71cbd0038e4e1f29aa8348a665953deb
tslai <tl@quartile.co>
2020-02-20 14:51:32 +0800

    [ADD] black, isort

--- base_users_list_views/models/res_users.py
+++ base_users_list_views/models/res_users.py
@@ -14,13 +14,8 @@ class ResUsers(models.Model):
         related="partner_id.customer", readonly=True, store=True
     )
     show_password = fields.Char(readonly=True, String="Password")
-    is_timecheck_light_date = fields.Datetime(
-        string='Is TCL Since',
-        default=False
-    )
-    is_timecheck_light = fields.Boolean(
-        string='TCL Paid',
-    )
+    is_timecheck_light_date = fields.Datetime(string="Is TCL Since", default=False)
+    is_timecheck_light = fields.Boolean(string="TCL Paid")
     has_supplier_access = fields.Boolean(
         string="Has Supplier Access", compute="compute_has_supplier_access", store=True
     )
@@ -30,8 +25,7 @@ class ResUsers(models.Model):
     def compute_has_supplier_access(self):
         for user in self:
             user.has_supplier_access = (
-                True if user.has_group(
-                    "supplier_user_access.group_supplier") else False
+                True if user.has_group("supplier_user_access.group_supplier") else False
             )
 
     # object action for chrono update button in sale order form view
@@ -62,25 +56,29 @@ class ResUsers(models.Model):
 
     @api.multi
     def write(self, vals):
-        if 'is_timecheck_light' in vals and vals['is_timecheck_light']:
-            vals['is_timecheck_light_date'] = fields.datetime.now()
+        if "is_timecheck_light" in vals and vals["is_timecheck_light"]:
+            vals["is_timecheck_light_date"] = fields.datetime.now()
         return super(ResUsers, self).write(vals)
 
     def action_assign_supplier(self):
         vals = {}
         # Assign internal group to the user
         user_types_group_name = self.get_security_group_field_name(
-            'base.module_category_user_type')
-        internal_user_group_field_value = self.env.ref('base.group_user').id
+            "base.module_category_user_type"
+        )
+        internal_user_group_field_value = self.env.ref("base.group_user").id
         vals[user_types_group_name] = internal_user_group_field_value
         # Assign supplier group to the user
         supplier_group_name = self.get_security_group_field_name(
-            'supplier_user_access.module_category_supplier')
+            "supplier_user_access.module_category_supplier"
+        )
         supplier_group_field_value = self.env.ref(
-            'supplier_user_access.group_supplier').id
+            "supplier_user_access.group_supplier"
+        ).id
         vals[supplier_group_name] = supplier_group_field_value
         # Remove timecheck group from the user
         supplier_group_name = self.get_security_group_field_name(
-            'website_timecheck.module_category_timecheck')
+            "website_timecheck.module_category_timecheck"
+        )
         vals[supplier_group_name] = False
         return self.write(vals)
--- website_timecheck/models/res_users.py
+++ website_timecheck/models/res_users.py
@@ -9,34 +9,41 @@ class ResUsers(models.Model):
     _inherit = "res.users"
 
     timecheck_group = fields.Selection(
-        [('group_timecheck_trial', 'Trial'),
-         ('group_timecheck_basic', 'Basic'),
-         ('group_timecheck_light', 'Light'), ],
-        string='Groups',
+        [
+            ("group_timecheck_trial", "Trial"),
+            ("group_timecheck_basic", "Basic"),
+            ("group_timecheck_light", "Light"),
+        ],
+        string="Groups",
     )
 
     def get_security_group_field_name(self, group_category_xml_id):
-        for app, kind, gs in self.env['res.groups'].get_groups_by_application():
+        for app, kind, gs in self.env["res.groups"].get_groups_by_application():
             module_category = self.env.ref(group_category_xml_id)
             if app == module_category:
-                return 'sel_groups_' + '_'.join(map(str, map(int, gs)))
+                return "sel_groups_" + "_".join(map(str, map(int, gs)))
 
     def action_assign_timecheck(self):
-        self.update({'timecheck_group': 'group_timecheck_trial'})
+        self.update({"timecheck_group": "group_timecheck_trial"})
 
     @api.multi
     def write(self, vals):
         timecheck_group_name = self.get_security_group_field_name(
-            'website_timecheck.module_category_timecheck')
+            "website_timecheck.module_category_timecheck"
+        )
         # Assign Timecheck group by timecheck_group
-        if 'timecheck_group' in vals:
+        if "timecheck_group" in vals:
             # Assign Portal group to the user
             user_types_group_name = self.get_security_group_field_name(
-                'base.module_category_user_type')
-            portal_group_field_value = self.env.ref('base.group_portal').id
+                "base.module_category_user_type"
+            )
+            portal_group_field_value = self.env.ref("base.group_portal").id
             vals[user_types_group_name] = portal_group_field_value
             # Assign the selected Timecheck group to the user
-            timecheck_group_field_value = self.env.ref(
-                'website_timecheck.%s' % vals['timecheck_group']).id if vals['timecheck_group'] else False
+            timecheck_group_field_value = (
+                self.env.ref("website_timecheck.%s" % vals["timecheck_group"]).id
+                if vals["timecheck_group"]
+                else False
+            )
             vals[timecheck_group_name] = timecheck_group_field_value
         return super(ResUsers, self).write(vals)
