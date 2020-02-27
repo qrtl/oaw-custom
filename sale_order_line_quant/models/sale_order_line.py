7d83231e791030d422fc620e2ea82b64c3608714
tslai <tl@quartile.co>
2020-02-27 11:54:20 +0800

    [FIX] Add quant's sale order update triggers

--- sale_order_line_quant/models/sale_order_line.py
+++ sale_order_line_quant/models/sale_order_line.py
@@ -9,9 +9,12 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     quant_id = fields.Many2one("stock.quant", string="Stock Quant", copy=False)
-    lot_id = fields.Many2one(related="quant_id.lot_id", string="Case No.", store=True)
-    stock_owner_id = fields.Many2one(related="quant_id.owner_id", string="Stock Owner")
-    is_mto = fields.Boolean(related="order_id.is_mto", store=True, string="Is MTO?")
+    lot_id = fields.Many2one(related="quant_id.lot_id",
+                             string="Case No.", store=True)
+    stock_owner_id = fields.Many2one(
+        related="quant_id.owner_id", string="Stock Owner")
+    is_mto = fields.Boolean(related="order_id.is_mto",
+                            store=True, string="Is MTO?")
     quant_price_unit = fields.Float(related="lot_id.price_unit", string="Cost")
 
     @api.onchange("quant_id")
@@ -30,7 +33,8 @@ class SaleOrderLine(models.Model):
 
     @api.multi
     def _prepare_procurement_values(self, group_id=False):
-        values = super(SaleOrderLine, self)._prepare_procurement_values(group_id)
+        values = super(
+            SaleOrderLine, self)._prepare_procurement_values(group_id)
         self.ensure_one()
         values.update({"quant_id": self.quant_id.id, "lot_id": self.lot_id.id})
         return values
@@ -49,3 +53,37 @@ class SaleOrderLine(models.Model):
                         "for products tracking with Serial Number"
                     )
                 )
+
+    @api.multi
+    def write(self, vals):
+        if "quant_id" in vals:
+            for order_line in self:
+                order_line.quant_id.sudo().update({"sale_order_id": False})
+                if vals["quant_id"]:
+                    quant = self.env["stock.quant"].browse(
+                        vals["quant_id"]).sudo().update({
+                            "sale_order_id": order_line.order_id.id
+                        })
+        return super(SaleOrderLine, self).write(vals)
+
+    @api.model
+    def create(self, vals):
+        res = super(SaleOrderLine, self).create(vals)
+        if "quant_id" in vals and vals["quant_id"]:
+            res.quant_id.sudo().update({
+                "sale_order_id": res.order_id.id
+            })
+        return res
+
+    @api.multi
+    def unlink(self):
+        quant_ids = self.env["stock.quant"].browse()
+        for order_line in self:
+            if order_line.quant_id:
+                quant_ids += order_line.quant_id
+        res = super(SaleOrderLine, self).unlink()
+        if quant_ids:
+            quant_ids.sudo().update({
+                "sale_order_id": False
+            })
+        return res
