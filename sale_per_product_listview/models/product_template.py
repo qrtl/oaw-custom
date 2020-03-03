7d4c72bc043a2b7888871318dfbb1159c2c56d38
eHanseFrank <frank.schumacher@ehanse.de>
2020-03-03 07:53:06 +0000

    Futerh advancedments. Initializing works, but pt in self shows only one record

--- sale_per_product_listview/models/product_template.py
+++ sale_per_product_listview/models/product_template.py
@@ -34,49 +34,49 @@ class product_template_ext(models.Model):
                 pt.average = pt.total / pt.counts
 
     # triggered by More Button
-    # @api.multi
-    # def _initialize_values(self, pts):
-    #     sol_object = self.env['sale.order.line']
-    #     for pt in pts:
-    #         domain = [
-    #             ('product_id', '=', pt.product_variant_ids.ids),
-    #             ('state', '=', 'sale'),
-    #         ]
-    #         sols = sol_object.search(domain)
-    #         sols_len = len(sols)
-    #         if sols_len != 0:
-    #             pt.total = 0.0
-    #             pt.counts = sols_len
-    #             for sol in sols:
-    #                 date = sol.order_id.date_order
-    #                 rate = 1.0
-    #                 if date and sol.order_id.currency_id != self.env.user.company_id.currency_id:
-    #                     rate = self.env['res.currency.rate'].search([
-    #                         ('currency_id', '=', sol.order_id.currency_id.id),
-    #                         ('name', '<=', date),
-    #                     ], order='name desc', limit=1).rate or 1.0
+    @api.multi
+    def _initialize_values(self, pts):
+        sol_object = self.env['sale.order.line']
+        for pt in pts:
+            domain = [
+                ('product_id', '=', pt.product_variant_ids.ids),
+                ('state', '=', 'sale'),
+            ]
+            sols = sol_object.search(domain)
+            sols_len = len(sols)
+            if sols_len != 0:
+                pt.total = 0.0
+                pt.counts = sols_len
+                for sol in sols:
+                    date = sol.order_id.date_order
+                    rate = 1.0
+                    if date and sol.order_id.currency_id != self.env.user.company_id.currency_id:
+                        rate = self.env['res.currency.rate'].search([
+                            ('currency_id', '=', sol.order_id.currency_id.id),
+                            ('name', '<=', date),
+                        ], order='name desc', limit=1).rate or 1.0
 
-    #                 sol.subtotal_hkd = sol.price_subtotal / rate
-    #                 print(sol.subtotal_hkd)
-    #                 # Updating pt's total
-    #                 pt.total += sol.subtotal_hkd
+                    sol.subtotal_hkd = sol.price_subtotal / rate
+                    print(sol.subtotal_hkd)
+                    # Updating pt's total
+                    pt.total += sol.subtotal_hkd
 
-    #             # Updating pt's average
-    #             pt.average = pt.total / pt.counts
-    #     return
+                # Updating pt's average
+                pt.average = pt.total / pt.counts
+        return
 
     @api.multi
     def action_view_sol_open(self):
         view_id = self.env.ref('sale_per_product_listview.sale_order_lines_tree').id
         print(view_id)
         return {
-            'name': 'Sales Order Lines',
+            'name': self.default_code,
             'view_mode': 'tree',
             'res_model': 'sale.order.line',
             'view_id': view_id,
             'type': 'ir.actions.act_window',
-            'res_id': self.id,
             'target': 'current',
+            'domain' : [("product_id","in", self.product_variant_ids.ids)]
             
         }
 
--- sale_per_product_listview/wizards/__init__.py
+++ sale_per_product_listview/wizards/__init__.py
@@ -1,3 +1,1 @@
-# -*- coding: utf-8 -*-
-
 from . import products_sales_wizard
