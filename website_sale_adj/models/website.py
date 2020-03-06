f08c41711789ddcdce8e91fe21ba048b25014dd6
tslai <tl@quartile.co>
2020-03-06 09:57:59 +0800

    [IMP] Add wechat and whatsapp qr fields

--- website_sale_adj/models/website.py
+++ website_sale_adj/models/website.py
@@ -10,3 +10,15 @@ class Website(models.Model):
     about_us = fields.Text(string="About Us")
     contact_description = fields.Text(string="Contact Information")
     empty_page_message = fields.Text(string="Empty Shop Display Message")
+    whatsapp_link = fields.Char(
+        string='Whatsapp Link',
+    )
+    whatsapp_qr_image = fields.Binary(
+        string='Whatsapp QR Code',
+    )
+    wechat_link = fields.Char(
+        string='Wechat Link',
+    )
+    wechat_qr_image = fields.Binary(
+        string='Wechat QR Code',
+    )
