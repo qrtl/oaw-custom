d7ec15baf3eaac8798fec8842f1f7cfb5b3cd7df
Tim <tl@quartile.co>
2020-03-13 14:50:34 +0800

    [IMP] Add image to picking form view (#533)

--- stock_view_adjust_oaw/__manifest__.py
+++ stock_view_adjust_oaw/__manifest__.py
@@ -20,6 +20,7 @@
         "security/ir.model.access.csv",
         "views/stock_move_line_views.xml",
         "views/stock_move_views.xml",
+        "views/stock_picking_views.xml",
         "views/stock_quant_views.xml",
     ],
 }
