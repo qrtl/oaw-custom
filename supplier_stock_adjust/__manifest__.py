fd67384acdfe2b25e0c389cc8f022f6c26b4d55c
Frank Schumacher <eHanseFrank@users.noreply.github.com>
2020-03-23 09:51:59 +0800

    creating module dealing with supplier input into stock quant list view (#545)

---
+++ supplier_stock_adjust/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Quartile Limted, Timeware Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Supplier Stock Adjust",
+    "version": "12.0.1.0.0",
+    "category": "Stock",
+    "author": "Quartile Limited, Timeware Limited",
+    "website": "https://www.quartile.co",
+    "description": """
+    """,
+    "license": "AGPL-3",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_quant_views.xml",
+    ],
+    "installable" : True,
+}
