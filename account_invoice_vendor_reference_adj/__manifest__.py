7cae6c11c5a4b67e35ce6f1a8cf65fc43a93d652
Tim <tl@quartile.co>
2020-03-23 10:41:18 +0800

    [ADD] account_invoice_vendor_reference_adj (#546)

---
+++ account_invoice_vendor_reference_adj/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Adjust Vendor Bill Reference",
+    "category": "Purchase",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "summary": """""",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "depends": ["account"],
+    "data": [],
+    "installable": True,
+}
