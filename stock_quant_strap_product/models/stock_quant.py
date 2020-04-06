2529eab0bd7033d7b23c3993c56f3ef9a59135c6
tslai <tl@quartile.co>
2020-04-06 13:12:36 +0800

    [ADD] stock_quant_strap_product

---
+++ stock_quant_strap_product/models/stock_quant.py
@@ -0,0 +1,11 @@
+# Copyright 2020 Quartile Limted
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo import fields, models
+
+
+class StockQuant(models.Model):
+    _inherit = "stock.quant"
+
+    strap_product_id = fields.Many2one(string="Strap")
+    strap_product_image = fields.Binary(related="strap_product_id.image_medium", string="Strap Image")
