cb6930695c5303e09b72c577f907ec575eb1e727
tslai <tl@quartile.co>
2020-04-06 13:28:53 +0800

    [FIX] python header

--- stock_quant_strap_product/__manifest__.py
+++ stock_quant_strap_product/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Quartile Limted
+# Copyright 2020 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock Quant Strap Product",
