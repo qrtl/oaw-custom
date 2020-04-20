cf028a23fcde81c8eca23f2a6ef05827ec89d8e9
Tim <tl@quartile.co>
2020-04-20 17:01:38 +0800

    [T907][IMP] Homepage, brand search and template adjustments  (#564)

---
+++ website_timecheck/models/res_config_settings.py
@@ -0,0 +1,16 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    homepage_url = fields.Char(
+        string="Homepage",
+        related="website_id.homepage_url",
+        readonly=False,
+        help="Defines the homepage for the website, leave it empty to be set "
+        "as default ('/')",
+    )
