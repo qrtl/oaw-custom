17e10167e8399376e4cb8eb55e93890224a40a8e
tslai <tl@quartile.co>
2020-05-08 09:51:47 +0800

    [ADD] product_additional_info

---
+++ product_additional_info/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Product Additonal Information",
+    "version": "12.0.1.0.0",
+    "category": "Product",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "license": "LGPL-3",
+    "depends": ["product"],
+    "data": ["views/product_template_views.xml"],
+    "installable": True,
+}
