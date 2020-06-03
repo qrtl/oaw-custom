6a2f21618e37a6339f742e6f8b4a016c335eb873
Tim <tl@quartile.co>
2020-06-03 14:43:57 +0800

    [1047][IMP] Add _order attribute to stock.production.lot (#577)

--- stock_move_line_quant/__manifest__.py
+++ stock_move_line_quant/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2019 Quartile Limited
+# Copyright 2019-2020 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Quant/Serial Number on Stock Move Line",
     "category": "Stock",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.1.0",
     "license": "LGPL-3",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
