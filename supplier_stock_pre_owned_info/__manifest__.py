c122f6ece137fcf97a3a26aa324eeaae8baccecb
Tim <tl@quartile.co>
2020-08-11 14:14:36 +0800

    [972][IMP] Add pre-owned information fields to supplier access (#583)

--- supplier_stock_pre_owned_info/__manifest__.py
+++ supplier_stock_pre_owned_info/__manifest__.py
@@ -1,13 +1,13 @@
 # Copyright 2020 Quartile Limited
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Pre-Owned Products Features",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "category": "Stock",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "license": "LGPL-3",
-    "depends": ["supplier_stock"],
+    "depends": ["supplier_user_access"],
     "data": [
         "data/product_condition_data.xml",
         "data/product_parts_status_data.xml",
