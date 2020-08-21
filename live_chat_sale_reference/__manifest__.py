2b89180526ac8210ac7e1714f70465e204d219dc
YogeshMahera-SerpentCS <yogesh.mahera.serpentcs@gmail.com>
2020-08-21 17:36:58 +0530

    [IMP]Improved Code

--- live_chat_sale_reference/__manifest__.py
+++ live_chat_sale_reference/__manifest__.py
@@ -1,11 +1,11 @@
 # Copyright 2020 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    "name": "Live Chat Adjustment",
+    "name": "Live Chat Sale Reference",
     "version": "12.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
-    "category": "Website",
+    "category": "Sales",
     "license": "LGPL-3",
     "summary": "",
     "depends": ["sale"],
---
+++ live_chat_sale_reference/models/mail_channel.py
@@ -0,0 +1,25 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+import re
+
+from odoo import api, models
+
+
+class MailChannel(models.Model):
+    _inherit = "mail.channel"
+
+    @api.multi
+    @api.returns("mail.message", lambda value: value.id)
+    def message_post(self, message_type='notification', **kwargs):
+        keywords = re.findall(r"\w+", kwargs.get("body"))
+        sale_order_ids = self.env["sale.order"].search(
+            [("name", "in", keywords)])
+        for order in sale_order_ids:
+            kwargs.update({
+                'body':
+                    kwargs.get('body').replace(
+                        order.name,
+                        str('<a href="#" data-oe-id='+ str(order.id)+ ' data-oe-model="sale.order">'+ order.name+ "</a> "))})
+        return super(MailChannel, self).message_post(
+            message_type=message_type, **kwargs)
