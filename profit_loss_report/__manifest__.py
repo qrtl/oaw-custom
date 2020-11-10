7a706491726df27d2d55fec12ae1d510bdc7be69
Tim <tl@quartile.co>
2020-11-10 14:26:33 +0800

    [1409][IMP] Adjust groups of Reporting menu (#591)

--- profit_loss_report/__manifest__.py
+++ profit_loss_report/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Profit and Loss Report",
-    "version": "12.0.1.1.0",
+    "version": "12.0.1.1.1",
     "license": "AGPL-3",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
