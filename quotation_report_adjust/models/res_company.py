2b354b08ea5a18769aa98ab5ff1b9a7db1607d3c
Tim <tl@quartile.co>
2021-02-11 11:32:01 +0800

    [1226][UPD] Update timeware address (#588)

--- quotation_report_adjust/models/res_company.py
+++ quotation_report_adjust/models/res_company.py
@@ -9,3 +9,5 @@ class ResComopany(models.Model):
     _inherit = "res.company"
 
     alternative_logo = fields.Binary("Alternative Logo")
+    report_sino_address = fields.Html("Sino Quotation Address")
+    report_timeware_address = fields.Html("Timeware Quotation Address")
